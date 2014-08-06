a31c73943d5f94324075808c1cbcb697cd610bd1
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-05 23:30:05 -0400

    JSlinted

--- JavaScript/Language Features/Functions/src/higherOrderFunctions.js
+++ JavaScript/Language Features/Functions/src/higherOrderFunctions.js
@@ -1,33 +1,33 @@
 var pricelist = [10, 20, 30, 40, 50, 60, 70];
 
 var totalPrices = function (prices) {
-	var total = 0;
-	for (var i = 0; i < prices.length; i++) {
-		total += prices[i];
-	}
-	return total;
+    var total = 0;
+    for (var i = 0; i < prices.length; i++) {
+        total += prices[i];
+    }
+    return total;
 };
 
 
 var totalPricesOver30 = function (prices) {
-	var total2 = 0;
-	for (var i = 0; i < prices.length; i++) {
-		if (prices[i] > 30) {
-			total2 += prices[i];
-		}
-	}
-	return total2;
-}
+    var total2 = 0;
+    for (var i = 0; i < prices.length; i++) {
+        if (prices[i] > 30) {
+            total2 += prices[i];
+        }
+    }
+    return total2;
+};
 
 var totalPricesLess30 = function (prices) {
-	var total2 = 0;
-	for (var i = 0; i < prices.length; i++) {
-		if (prices[i] < 30) {
-			total2 += prices[i];
-		}
-	}
-	return total2;
-}
+    var total2 = 0;
+    for (var i = 0; i < prices.length; i++) {
+        if (prices[i] < 30) {
+            total2 += prices[i];
+        }
+    }
+    return total2;
+};
 
 console.log(totalPrices(pricelist));
 console.log(totalPricesOver30(pricelist));
@@ -35,16 +35,16 @@ console.log(totalPricesLess30(pricelist));
 
 // Higher Order Function
 var totalPricesNew = function (prices, selector) {
-	var total = 0;
-	for (var i = 0; i < prices.length; i++) {
-		if(selector(prices[i])) {
-			total += prices[i];
-		}
-	};
+    var total = 0;
+    for (var i = 0; i < prices.length; i++) {
+        if(selector(prices[i])) {
+            total += prices[i];
+        }
+    }
 
-	return total;
-}
+    return total;
+};
 
-console.log(totalPricesNew(pricelist, function(price) { return true }));
-console.log(totalPricesNew(pricelist, function(price) { return price > 30}));
-console.log(totalPricesNew(pricelist, function(price) { return price < 30}));
+console.log(totalPricesNew(pricelist, function(price) { return true; }));
+console.log(totalPricesNew(pricelist, function(price) { return price > 30;}));
+console.log(totalPricesNew(pricelist, function(price) { return price < 30;}));
