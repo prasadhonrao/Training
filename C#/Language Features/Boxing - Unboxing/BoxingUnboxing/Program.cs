0f0e1303de27d971c5799ca4c4976d6e7456ecc5
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-20 10:09:35 -0400

    Initial commit

---
+++ C#/Language Features/Boxing - Unboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 100;
+
+            object age = counter; // boxing
+
+            DisplayValue(counter); // boxing
+
+            Console.ReadLine();
+        }
+
+        public static void DisplayValue(object value)
+        {
+            int local = (int)value; //unboxing
+        }
+    }
+}
