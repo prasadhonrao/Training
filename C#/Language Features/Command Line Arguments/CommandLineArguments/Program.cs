2bd8a035227a15d3dd20bcb745a8670b72dbf832
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-20 10:06:05 -0400

    Initial commit

---
+++ C#/Language Features/Command Line Arguments/CommandLineArguments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ensure commmand line arguments are passed
+            if (args != null && args.Length > 0)
+            {
+                foreach (var item in args)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
