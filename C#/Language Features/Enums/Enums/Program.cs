78c186207937407f959956e5ec9db495a7929368
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-21 15:37:22 -0400

    Initial commit

---
+++ C#/Language Features/Enums/Enums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var color = RainbowColor.Red;
+            Console.WriteLine(color);
+
+            var emp = new Employee();
+            emp.Type = EmployeeType.Permanent;
+            Console.WriteLine("Employee is of {0} type", emp.Type);
+
+            Console.ReadLine();
+        }
+    }
+
+    public enum RainbowColor
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+
+    public enum EmployeeType
+    {
+        Temporary = 1,
+        Contractor = 34,
+        Permanent = 100
+    }
+}
---
+++ C#/Language Features/Enums/Index.txt
@@ -0,0 +1,2 @@
+Enums
+Initialization
