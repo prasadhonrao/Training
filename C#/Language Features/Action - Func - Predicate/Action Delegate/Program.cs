8a062cddd28b4505652eb27701b8fa5eaa192178
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-21 10:33:01 -0400

    Initial commit

---
+++ C#/Language Features/Action - Func - Predicate/Action Delegate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Training.CSharp
+{
+    delegate void LogMessage();
+    delegate void LogCustomMessage(string message);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LogMessage delegate
+            
+            LogMessage lm = () =>
+            {
+                Console.WriteLine("Logging using LogMessage delegate");
+            };
+
+            lm();
+            
+            #endregion
+
+            #region LogCustomMessage delegate
+
+            LogCustomMessage lcm = (s) =>
+            {
+                Console.WriteLine(s);
+            };
+
+            lcm("Log this message using LogCustomMessage delegate");
+
+            #endregion
+
+            #region LogMessage Action delegate
+            
+            Action<string> LogMessageAction = (message) => Console.WriteLine(message);
+            LogMessageAction("With Action delegate"); 
+
+            #endregion
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ C#/Language Features/Action - Func - Predicate/Func Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Training.CSharp
+{
+    public delegate int AddDelegate(int x, int y);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delegate with method reference
+            AddDelegate addMethod = new AddDelegate(AddMethod);
+            var result1 = addMethod(10, 20);
+            Console.WriteLine("Delegate with method reference " + result1);
+
+            // Delegate with lambda
+            AddDelegate addLambda = (x, y) => x + y;
+            int result2 = addLambda(10, 20);
+            Console.WriteLine("Delegate with lambda " + result2);
+
+            // Func
+            Func<int, int, int> addFunc = (x, y) => { return x + y; };
+            int result3 = addFunc(10, 20);
+            Console.WriteLine("Func Delegate " + result3);
+
+            Console.ReadLine();
+        }
+
+        private static int AddMethod(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
