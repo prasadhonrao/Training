115a22f832a382ebdd015e3b9850ed0bdc552625
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-26 11:43:25 -0400

    Initial commit

---
+++ C#/Language Features/DLR/Excel Automation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExcelAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type excelApp = Type.GetTypeFromProgID("Excel.Application");
+            dynamic excel = Activator.CreateInstance(excelApp);
+            excel.Visible = true;
+
+            excel.Workbooks.Add();
+            dynamic sheet = excel.ActiveSheet;
+
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                sheet.Cells[i + 1, "A"] = processes[i].ProcessName;
+                sheet.Cells[i + 1, "B"] = processes[i].Threads.Count;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Language Features/DLR/Expando/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic employee = new ExpandoObject();
+
+            // Adding dynamic property
+            employee.Name = "Prasad Honrao";
+            Console.WriteLine(employee.Name);
+
+            // Adding dynamic method
+            employee.Speak = new Action(() => Console.WriteLine(employee.Name));
+            employee.Speak();
+
+            Console.WriteLine("\nIterating over expando object items");
+            foreach (var item in employee)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
