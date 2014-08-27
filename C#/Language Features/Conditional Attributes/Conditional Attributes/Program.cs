ebda0911e6474d0d1f53b1efb9495e7bbd5fa96d
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-27 13:43:01 -0400

    Initial commit

---
+++ C#/Language Features/Conditional Attributes/Conditional Attributes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            WriteMessage("Application is running in debug mode");
+#elif RELEASE
+                WriteMessage("App is running in release mode");
+#else
+                WriteMessage("Check the app mode");
+#endif
+
+#if CUSTOM
+            Console.WriteLine("You have added CUSTOM compilation symbol");
+#endif
+
+            Console.ReadLine();
+        }
+
+        public static void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
