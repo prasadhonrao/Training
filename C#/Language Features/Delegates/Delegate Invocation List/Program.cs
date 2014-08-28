2cc34e4303894df9eb6deff38f16b641d3a01b49
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-08-28 15:10:19 -0400

    Initial commit

---
+++ C#/Language Features/Delegates/Delegate Invocation List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        delegate string StringProcessor(string message);
+
+        static void Main(string[] args)
+        {
+            #region Delegate chaining with return value always returns the value of last action in invocation list
+            var one = new StringProcessor(StaticMethods.PrintOne);
+            var two = new StringProcessor(StaticMethods.PrintTwo);
+            var three = new StringProcessor(StaticMethods.PrintThree);
+
+            var combine = (StringProcessor)Delegate.Combine(one, two, three);
+            var result = combine("Hello");
+            Console.WriteLine(result); 
+            #endregion
+
+            #region Get invocation list
+            var list = combine.GetInvocationList();
+            foreach (StringProcessor item in list)
+            {
+                item("Test");
+            } 
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class StaticMethods
+    {
+        public static string PrintOne(string message)
+        {
+            return message + " One";
+        }
+
+        public static string PrintTwo(string message)
+        {
+            return message + " Two";
+        }
+
+        public static string PrintThree(string message)
+        {
+            return message + " Three";
+        }
+    }
+}
