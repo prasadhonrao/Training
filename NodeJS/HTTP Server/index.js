4ee74bc23e4d4014f46de9769d981ddebd6c2150
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-01 22:48:10 -0400

    'request' event handled while creating http server. Another way of defining event.

--- NodeJS/HTTP Server/index.js
+++ NodeJS/HTTP Server/index.js
@@ -5,4 +5,11 @@ var server = http.createServer(function(req, res){
     res.end("<html><head></head><body><h1>" + req.url + "</h1></body></html>");
 });
 
+// Another way of creating HTTP server and listening to 'request' event
+// var server = http.createServer();
+// server.on('request', function(req, res){
+// 	console.log(req.url);
+//     res.end("<html><head></head><body><h1>" + req.url + "</h1></body></html>");
+// })
+
 server.listen(3000);
