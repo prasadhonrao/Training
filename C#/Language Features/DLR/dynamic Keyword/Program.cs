41c1fabca5ebfade3214be1c58b087b065148374
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-03 14:39:13 -0400

    Initial commit

---
+++ C#/Language Features/DLR/dynamic Keyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Hello World!";
+            Console.WriteLine("Text length is {0}", text.Length);
+
+            dynamic content = "Hello World!";
+            Console.WriteLine("Content length is {0}", content.Length);
+
+            content = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine("Content length is {0}", content.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
