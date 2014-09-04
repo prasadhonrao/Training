8d3e050e9f3827d5fb2846b0ae0decb0379318f4
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-04 11:10:03 -0400

    Initial commit

---
+++ C#/Language Features/Operators/Bitwise Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Training.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 10; //1010
+
+            // left shift by 2 bits
+            var leftShift = number << 2; //101000
+            Console.WriteLine(leftShift);
+
+            // right shift by 2 bits
+            var rightShift = number >> 2; //0010
+            Console.WriteLine(rightShift);
+        }
+    }
+}
