4c2ed4a76431e2391fd20c3dbfb3991856c460c8
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-09 11:33:41 -0400

    Initial commit

---
+++ C#/Language Features/Cultures/Cultures/CultureTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.Threading;
+
+namespace Cultures
+{
+    [TestClass]
+    public class CultureTests
+    {
+        [TestMethod]
+        public void Turkish_Culture_Fail_Test()
+        {
+            CultureInfo turkish = CultureInfo.CreateSpecificCulture("tr");
+            Thread.CurrentThread.CurrentCulture = turkish;
+            var input = "mail";
+            var expected = "MAIL";
+            Assert.AreNotEqual(expected, input.ToUpper()); // Note AreNotEqual
+        }
+
+        [TestMethod]
+        public void Turkish_Culture_Pass_Test()
+        {
+            CultureInfo turkish = CultureInfo.CreateSpecificCulture("tr");
+            Thread.CurrentThread.CurrentCulture = turkish;
+            var input = "mail";
+            var expected = "MAIL";
+            Assert.AreEqual(expected, input.ToUpperInvariant()); // Note AreEqual. ToUpperInvariant ignores the culture
+        }
+    }
+}
