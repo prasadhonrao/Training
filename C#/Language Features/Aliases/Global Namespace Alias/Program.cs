4f9ca637d498294a4c5999f60bc71fa85ddc42f4
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-10 09:59:20 -0400

    Initial commit

---
+++ C#/Language Features/Aliases/Global Namespace Alias/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//global class
+class Configuration {}
+
+namespace Global.Namespace.Alias
+{
+    // local class to namespace
+    class Configuration { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(Configuration));
+            Console.WriteLine(typeof(global::Configuration));
+            Console.WriteLine(typeof(global::Global.Namespace.Alias.Configuration)); // Nested class can still be accessed using global identifier
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Language Features/Aliases/Namespace Aliases/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WebForms = System.Web.UI.WebControls;
+using WinForms = System.Windows.Forms;
+
+namespace Namespace.Aliases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(WinForms.Button));
+            Console.WriteLine(typeof(WebForms.Button));
+        }
+    }
+}
