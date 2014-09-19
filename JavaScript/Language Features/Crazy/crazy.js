4b8fcd8b1cb05e4811e3b854e57c08bdd8505201
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-09-19 15:19:32 -0400

    variable overlap example added

--- JavaScript/Language Features/Crazy/crazy.js
+++ JavaScript/Language Features/Crazy/crazy.js
@@ -2,3 +2,10 @@ console.log(0.1 + 0.2 === 0.3);
 
 console.log(Math.min() < Math.max());
 
+var es = [];
+for (var i = 0; i < 10; i++) {
+  es[i] = function () {
+    console.log("Upcoming edition of ECMAScript is ES" + i);
+  };
+}
+es[6](); // Upcoming edition of ECMAScript is ES10
