64f1774978f72980b35c21151a4878de68a72115
Prasad Honrao <Honrao.Prasad@Gmail.com>
2014-12-29 14:42:24 -0500

    Initial commit

---
+++ C#/Messaging Patterns/ZeroMQ - Fire & Forget/ZeroMQ - Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZMQ;
+
+namespace ZeroMQ___Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press [Enter] key to send the messages");
+            Console.ReadLine();
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var context = new Context();
+            using (var client = context.Socket(SocketType.PUSH))
+            {
+                client.Connect("tcp://127.0.0.1:5555");
+                for (int i = 0; i < 1000; i++)
+                {
+                    client.Send("Message " + i, Encoding.UTF8);
+                }
+            }
+
+            watch.Stop();
+            Console.WriteLine("Total time elapsed " + watch.ElapsedMilliseconds);
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ C#/Messaging Patterns/ZeroMQ - Fire & Forget/ZeroMQ - Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZMQ;
+
+namespace ZeroMQ___Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press [Enter] key to read the messages");
+            Console.ReadLine();
+
+            var context = new Context();
+            using (var server = context.Socket(SocketType.PULL))
+            {
+                server.Bind("tcp://127.0.0.1:5555");
+                while (true)
+                {
+                    var message = server.Recv(Encoding.UTF8);
+                    Console.WriteLine(message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
