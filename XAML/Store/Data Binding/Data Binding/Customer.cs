83cd4567db07eb33a6d4bccd3a79d0f1711b06b5
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-01-06 00:34:38 -0500

    Initial commit

---
+++ XAML/Store/Data Binding/Data Binding/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Binding
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
