a1ca00d39ac0c38872bd82ac327e233c29e9f0be
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-01-07 00:03:08 -0500

    Initial commit

---
+++ XAML/Store/Page Navigation/Page Navigation/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_Navigation
+{
+    class Customer
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ XAML/Store/Page Navigation/Page Navigation/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Page_Navigation.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+
+        }
+
+        private string _title = "Page Navigation";
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Title"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
