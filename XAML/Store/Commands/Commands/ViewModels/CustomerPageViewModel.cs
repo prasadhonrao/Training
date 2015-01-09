71565436733c7523cad70ec0135394eb5d0445e3
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-01-08 21:49:19 -0500

    Initial commit

---
+++ XAML/Store/Commands/Commands/ViewModels/CustomerPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.ViewModels
+{
+    class CustomerPageViewModel : INotifyPropertyChanged
+    {
+        private Customer _customer;
+
+        public Customer Customer
+        {
+            get { return _customer; }
+            set
+            {
+                if (_customer != value)
+                {
+                    _customer = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Customer"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ XAML/Store/Commands/Commands/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Commands.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace Commands.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public RelayCommand<string> NavigateCommand { get; set; }
+
+
+        public MainPageViewModel()
+        {
+            NavigateCommand = new RelayCommand<string>(OnExecute, OnCanExecute);
+        }
+
+        private bool OnCanExecute(string input)
+        {
+            if (input.Trim().Length == 0)
+                return false;
+            return true;
+        }
+
+        private async void OnExecute(string input)
+        {
+            var messageDialog = new MessageDialog(input);
+            await messageDialog.ShowAsync();
+        }
+
+        private string _title = "";
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Title"));
+                    NavigateCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        //private void OnTitleChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    NavigateCommand.RaiseCanExecuteChanged();
+        //}
+    }
+}
