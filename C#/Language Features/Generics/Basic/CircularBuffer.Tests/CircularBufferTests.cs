43f6bf8e0da0fd81290ae3538636b3fe8033b475
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-05-12 15:42:15 +0530

    Initial commit

---
+++ C#/Language Features/Generics/Basic/CircularBuffer.Tests/CircularBufferTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace CircularBuffer.Tests
+{
+    [TestClass]
+    public class CircularBufferTests
+    {
+        [TestMethod]
+        public void CircularBuffer_IsFullTest()
+        {
+            var buffer = new CircularBuffer<double>(3);
+            buffer.Add(1.2);
+            buffer.Add(2.9);
+            buffer.Add(10.76);
+            Assert.IsTrue(buffer.IsFull);
+
+            var stringBuffer = new CircularBuffer<string>(10);
+            stringBuffer.Add("One");
+            stringBuffer.Add("Two");
+            stringBuffer.Add("Three");
+            Assert.IsFalse(stringBuffer.IsFull);
+        }
+
+        [TestMethod]
+        public void CircularBuffer_AddTest()
+        {
+            var buffer = new CircularBuffer<double>(3);
+            buffer.Add(1.2);
+            buffer.Add(2.9);
+            buffer.Add(3.9);
+            Assert.IsTrue(buffer.Read() == 1.2);
+        }
+
+        [TestMethod]
+        public void CircularBuffer_ReadTest()
+        {
+            var buffer = new CircularBuffer<int>(3);
+            buffer.Add(1);
+            buffer.Add(2);
+            buffer.Add(3);
+            Assert.IsTrue(buffer.Read() == 1);
+            Assert.IsTrue(buffer.Read() == 2);
+            Assert.IsTrue(buffer.Read() == 3);
+        }
+
+        [TestMethod]
+        public void CircularBuffer_OverrideTest()
+        {
+            var buffer = new CircularBuffer<int>(3);
+            buffer.Add(1);
+            buffer.Add(2);
+            buffer.Add(3);
+            buffer.Add(4);
+            Assert.IsTrue(buffer.IsFull);
+            Assert.IsTrue(buffer.Read() == 2);
+        }
+    }
+}
---
+++ C#/Language Features/Generics/Basic/CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularBuffer
+{
+    public class CircularBuffer<T>
+    {
+        private T[] buffer;
+        int start;
+        int end;
+
+        public CircularBuffer() : this(capacity: 10)
+        {
+
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            buffer = new T[capacity + 1];
+            start = 0;
+            end = 0;
+        }
+
+        public void Add(T value)
+        {
+            buffer[end] = value;
+            end = (end + 1) % buffer.Length;
+
+            if (end == start)
+            {
+                start = (start + 1) % buffer.Length;
+            }
+        }
+
+        public T Read()
+        {
+            T result = buffer[start];
+            start = (start + 1) % buffer.Length;
+            return result;
+        }
+
+        public bool IsFull
+        {
+            get { return (end + 1) % buffer.Length == start; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return end == start; }
+        }
+
+        public int Capacity
+        {
+            get { return buffer.Length; }
+        }
+
+
+    }
+}
