eb2e1b9190dd95802beb62fec45549e45b75a62b
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-05-14 15:36:16 +0530

    Initial commit

---
+++ C#/Language Features/Generics/Collections/ImmutableQueue/ImmutableQueue.Collection.Tests/ImmutableQueueTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+
+namespace ImmutableQueue.Collection.Tests
+{
+    [TestClass]
+    public class ImmutableQueueTests
+    {
+        [TestMethod]
+        public void ImmutableQueue_IsEmpty_Test()
+        {
+            var iqueue = ImmutableQueue<int>.Empty;
+            Assert.IsTrue(iqueue.IsEmpty);
+            var newQueue = iqueue.Enqueue(1);
+            Assert.IsTrue(iqueue.IsEmpty);
+            Assert.IsFalse(newQueue.IsEmpty);
+        }
+
+        [TestMethod]
+        public void ImmutableQueue_Add_Test()
+        {
+            var emptyFruitBasket = ImmutableQueue<string>.Empty;
+            var fruitBasket = emptyFruitBasket.Enqueue("Banana");
+            Assert.IsTrue(emptyFruitBasket.IsEmpty);
+            Assert.IsFalse(fruitBasket.IsEmpty);
+            Assert.IsTrue(fruitBasket.ToImmutableList().Count > 0);
+        }
+
+        [TestMethod]
+        public void ImmutableQueue_GetItem_Test()
+        {
+            var emptyFruitBasket = ImmutableQueue<string>.Empty;
+            var fruitBasket = emptyFruitBasket.Enqueue("Banana");
+            Assert.IsTrue(emptyFruitBasket.IsEmpty);
+            string fruit = string.Empty;
+            fruitBasket = fruitBasket.Dequeue(out fruit);
+            Assert.IsTrue(string.Equals(fruit, "Banana"));
+            Assert.IsTrue(fruitBasket.IsEmpty);
+        }
+
+    }
+}
---
+++ C#/Language Features/Generics/Collections/ImmutableQueue/ImmutableQueue.Collection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace ImmutableQueue.Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emplyeeIQueue = ImmutableQueue<string>.Empty;
+            emplyeeIQueue = emplyeeIQueue.Enqueue("Prasad");
+            emplyeeIQueue = emplyeeIQueue.Enqueue("Amit");
+            emplyeeIQueue = emplyeeIQueue.Enqueue("Scott");
+
+            Console.WriteLine("Employee Queue count {0}", emplyeeIQueue.Count());
+
+            foreach (var item in emplyeeIQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            string firstEmployee = string.Empty;
+            emplyeeIQueue = emplyeeIQueue.Dequeue(out firstEmployee);
+            Console.WriteLine("First number in queue {0}", firstEmployee);
+
+            Console.WriteLine("Number of remaining items in queue {0}", emplyeeIQueue.Count());
+
+            Console.ReadLine();
+        }
+    }
+}
