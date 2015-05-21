709f878c6a90560ca3bde200934c67466c2e2cca
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-05-21 17:25:57 +0530

    ConcurrentBag project added

---
+++ C#/Language Features/Generics/Collections/ConcurrentBag/ConcurrentBag.Collection.Tests/ConcurrentBagTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Concurrent;
+
+namespace ConcurrentBag.Collection.Tests
+{
+    // Unlike Stack or Queue, Order of retrieving item from ConcurrentBag 
+    // is not fixed in multithreaded environment.
+
+    [TestClass]
+    public class ConcurrentBagTests
+    {
+        [TestMethod]
+        public void ConcurrentBag_Add_Test()
+        {
+            var cb = new ConcurrentBag<string>();
+            cb.Add("Red");
+            cb.Add("Orange");
+            cb.Add("Yellow");
+            Assert.IsTrue(cb.Count == 3);
+        }
+
+        [TestMethod]
+        public void ConcurrentBag_TryTake_Test()
+        {
+            var cb = new ConcurrentBag<string>();
+            cb.Add("Red");
+            cb.Add("Orange");
+            cb.Add("Yellow");
+
+            string result;
+            var success = cb.TryTake(out result);
+
+            Assert.IsTrue(cb.Count == 2);
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void ConcurrentBag_TryPeek_Test()
+        {
+            var cb = new ConcurrentBag<string>();
+            cb.Add("Red");
+            cb.Add("Orange");
+            cb.Add("Yellow");
+
+            string result;
+            var success = cb.TryPeek(out result);
+
+            Assert.IsTrue(cb.Count == 3);
+            Assert.IsTrue(success);
+        }
+    }
+}
