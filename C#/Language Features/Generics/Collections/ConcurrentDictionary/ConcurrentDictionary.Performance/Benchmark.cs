2fef03800e69723cd6cf5e6e04a01e08e4cf4562
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-05-22 13:49:45 +0530

    ConcurrentDictionary performance example added

---
+++ C#/Language Features/Generics/Collections/ConcurrentDictionary/ConcurrentDictionary.Performance/Benchmark.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentDictionary.Performance
+{
+    public class Benchmark
+    {
+        public static void Add(IDictionary<int, int> dictionary, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                dictionary.Add(i, i);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                dictionary[i] = i + 1;
+            }
+        }
+    }
+}
--- C#/Language Features/Generics/Collections/ConcurrentDictionary/ConcurrentDictionary.Performance/Program.cs
+++ C#/Language Features/Generics/Collections/ConcurrentDictionary/ConcurrentDictionary.Performance/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,25 @@ namespace ConcurrentDictionary.Performance
     {
         static void Main(string[] args)
         {
+            var size = 1000000;
+            var dictionary = new Dictionary<int, int>();
+            var concurrent = new ConcurrentDictionary<int, int>();
+            
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Benchmark.Add(dictionary, size);
+            watch.Stop();
+            var dictionaryTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Ellapsed time for Dictionary {0}", dictionaryTime);
+
+            watch.Reset();
+            watch.Start();
+            Benchmark.Add(concurrent, size);
+            watch.Stop();
+            var concurrentTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Ellapsed time for ConcurrentDictionary {0}", concurrentTime);
+
+            Console.ReadLine();
         }
     }
 }
