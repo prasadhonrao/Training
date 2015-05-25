efff0018744382a67a4da35a4d4163d4fd81f056
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-05-25 15:26:06 +0530

    Initial commit

---
+++ C#/Language Features/Structs/Auto Properties/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public struct Foo
+    {
+        public int value { get; private set; }
+
+        // You need to explicitely call this() from all the other constructor to ensure that all the fields in auto properties are properly initialized
+
+        public Foo(int value) : this()
+        {
+            this.value = value;
+        }
+    }
+}
