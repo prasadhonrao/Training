7aa58749839487754017f4706aeb6d75af99bb5b
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-03 10:27:15 +0530

    Task Result Harvest example added

---
+++ C#/Language Features/Task Parallel Library/Task Result Harvest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Result_Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<string> printTask = Task.Factory.StartNew(() => 
+            {
+                return PrintMessage();
+            });
+
+            //  .Result property implicitly calls .Wait
+            Console.WriteLine(printTask.Result);
+
+            Console.ReadLine();
+        }
+
+
+        static string PrintMessage()
+        {
+            Console.WriteLine("Work done!");
+            return "Work done message printed";
+        }
+    }
+}
