64f3a77d41acc3c20601c910cc51f91a26f36256
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-15 14:31:24 +0530

    Initial commit

---
+++ Entity Framework/SP Mapping - Return Entity/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_Mapping___Return_Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AWEntities())
+            {
+                var details = context.GetProductCategory(1).FirstOrDefault();
+                Console.WriteLine(details.Name);
+            }
+
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+    }
+}
