c6f486b2ac93d96a5b8e13bf8401fb2d71981fba
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-15 13:01:26 +0530

    Complex type example added

---
+++ Entity Framework/Complex Types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AWEntities())
+            {
+                var products = context.Products
+                                      .Where(p => p.ProductAttributes.Color == "Blue")
+                                      .Select(p => p.Name);
+                
+                foreach (var item in products)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
