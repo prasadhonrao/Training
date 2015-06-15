5dcd40e433b0a9673c7390f121208ae73be19dd1
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-15 14:45:33 +0530

    Initial commit

---
+++ Entity Framework/SP Mapping - Return Scalar Value/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_Mapping___Return_Scalar_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AWEntities())
+            {
+                ObjectResult<int?> count = context.GetProductCategoryCount();
+                Console.WriteLine("Product category count {0}", count.First().Value);
+            }
+
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+    }
+}
