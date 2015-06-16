3c6b2ed5d3f378dc2aa951cee9d3a690ab55a63c
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-16 14:57:54 +0530

    Inheritance - Table Per Hierarchy example added

---
+++ Entity Framework/Inheritance - Table Per Hierarchy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance___Table_Per_Hierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AWEntities())
+            {
+                Console.WriteLine("List of Active Vendors \n");
+                var activeVendor = context.Vendors.OfType<ActiveVendor>();
+                foreach (var vendor in activeVendor)
+                {
+                    Console.WriteLine(vendor.Name);
+                }
+            }
+
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+    }
+}
