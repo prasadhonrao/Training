3667b011e4b666d5262ebe90a54d84122a14b7d1
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-19 11:45:04 +0530

    Initial commit

--- Entity Framework/Database First - DbContext Methods/Properties/AssemblyInfo.cs
+++ Entity Framework/Database First - DbContext Methods/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeploymentSlot")]
+[assembly: AssemblyTitle("Database First - DbContext Methods")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CTS")]
-[assembly: AssemblyProduct("DeploymentSlot")]
+[assembly: AssemblyProduct("Database First - DbContext Methods")]
 [assembly: AssemblyCopyright("Copyright © CTS 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e5274ac-24a3-4c56-9c6c-ccf23f6ea93f")]
+[assembly: Guid("6a7c2ff5-1f33-44bf-b4d0-2a6a1f9b88ac")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
