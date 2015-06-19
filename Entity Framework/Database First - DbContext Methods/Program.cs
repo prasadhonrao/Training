4c8ef16528a3dd6f2cabd412dbd8eff823cdba1e
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-19 14:16:03 +0530

    Model and methods added

--- Entity Framework/Database First - DbContext Methods/Program.cs
+++ Entity Framework/Database First - DbContext Methods/Program.cs
@@ -10,6 +10,50 @@ namespace Database_First___DbContext_Methods
     {
         static void Main(string[] args)
         {
+            AddNewProductCategory();
+            UpdateProductCategory();
+            DeleteProductCategory();
+
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+
+        }
+
+        private static void AddNewProductCategory()
+        {
+            using (var context = new AWEntities())
+            {
+                var newCategory = new ProductCategory
+                {
+                    Name = "New Product Category"
+                };
+                context.ProductCategories.Add(newCategory);
+
+                Console.WriteLine(context.Entry(newCategory).State);
+            }
+        }
+        
+        private static void UpdateProductCategory()
+        {
+            using (var context = new AWEntities())
+            {
+                var existingCategory = context.ProductCategories.Find(1);
+                existingCategory.Name = "Updated Category";
+                Console.WriteLine(context.Entry(existingCategory).State);
+            }
+        }
+        
+        private static void DeleteProductCategory()
+        {
+            using (var context = new AWEntities())
+            {
+                var categoryToDelete = context.ProductCategories.Find(1);
+                if (categoryToDelete != null)
+                {
+                    context.ProductCategories.Remove(categoryToDelete);
+                    Console.WriteLine(context.Entry(categoryToDelete).State);
+                }
+            }
         }
     }
 }
