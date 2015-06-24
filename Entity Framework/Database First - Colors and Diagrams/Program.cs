7399ab656940f34c82309b525012452f58ec430b
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-24 11:52:00 +0530

    Database First - Colors and Diagrams project added

---
+++ Entity Framework/Database First - Colors and Diagrams/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First___Colors_and_Diagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
