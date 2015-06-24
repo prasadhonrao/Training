bb00bf44802b05acc742638034c2dcf5d174ddfc
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-24 12:28:12 +0530

    Database First - Enum Mapping added

---
+++ Entity Framework/Database First - Enum Mapping/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First___Enum_Mapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AWEntities())
+            {
+                var query = context.People.Where(p => p.EmailPromotion == EmailPromotionEnum.Low).Take(5);
+                foreach (var person in query)
+                {
+                    Console.WriteLine("{0} : {1}", person.EmailPromotion, person.FirstName);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
