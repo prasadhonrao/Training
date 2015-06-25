f1bb5a20825fa0be8737f058bec7f8acef4ea2b2
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-06-25 11:18:54 +0530

    Database First - Data Load Methods added

---
+++ Entity Framework/Database First - Data Load Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Database_First___Data_Load_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EagerLoad();
+            ExplicitLoad();
+        }
+
+        private static void ExplicitLoad()
+        {
+            using (var context = new AWEntities())
+            {
+                var customer = context.Customers.First();
+                context.Entry(customer).Collection(c => c.SalesOrderHeaders).Load();
+
+                Console.WriteLine("{0} : {1}", customer.AccountNumber, customer.SalesOrderHeaders.Count());
+            }
+        }
+
+        private static void EagerLoad()
+        {
+            using (var context = new AWEntities())
+            {
+                // Include lambda extension method needs System.Data.Entity using statement
+                var customersWithOrders = context.Customers.Include(c => c.SalesOrderHeaders).ToList();
+
+                foreach (var customer in customersWithOrders)
+                {
+                    Console.WriteLine("{0} : {1}", customer.AccountNumber, customer.SalesOrderHeaders.Count());
+                }
+            }
+        }
+    }
+}
