d27ebe3be927dcadbdbf5897195bd66998f3eafd
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-07-03 14:47:47 +0530

    Operators added

---
+++ JavaScript/Language Features/Operators/src/operators.js
@@ -0,0 +1,34 @@
+var firstNumber = 10;
+var secondNumber = 4;
+var result = 0;
+
+result = firstNumber + secondNumber;
+console.log("Addition : "  + result);
+
+result = firstNumber - secondNumber;
+console.log("Subtraction : "  + result);
+
+result = firstNumber * secondNumber;
+console.log("Multiplication : "  + result);
+
+result = firstNumber / secondNumber;
+console.log("Division : "  + result);
+
+result = firstNumber % secondNumber;
+console.log("Mod : "  + result);
+
+firstNumber = 10;
+result = ++firstNumber;
+console.log("Pre Increment : "  + result);
+
+firstNumber = 10;
+result = firstNumber++;
+console.log("Post Increment : "  + result);
+
+firstNumber = 10;
+result = --firstNumber;
+console.log("Pre Decrement : "  + result);
+
+firstNumber = 10;
+result = firstNumber--;
+console.log("Post Decrement : "  + result);
