37a16aab8063de9201a0f46551af11b7974e4e2a
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-07-11 11:30:53 +0530

    comments added

--- JavaScript/Language Features/Functions/src/arguments.js
+++ JavaScript/Language Features/Functions/src/arguments.js
@@ -1,3 +1,5 @@
+/* arguments is used to get access to variable number of arguments passed while calling the function*/
+
 function getArgumentsLength() {
     console.log("number of arguments - " + arguments.length);
 }
@@ -6,6 +8,8 @@ getArgumentsLength();
 getArgumentsLength(1);
 getArgumentsLength(1, '');
 getArgumentsLength(1, '', 12 / 12 / 2012);
+
+/* Use of arguments to calculate the total of variable number of arguments*/
 
 function add() {
     var total = 0;
@@ -19,12 +23,15 @@ console.log(add(1));
 console.log(add(1, 2));
 console.log(add(1, 2, 'wow, its working for string also!','£'));
 
+/* Find missing arguments using if*/
+
 function foo(one, two, three) {
     if (two) console.log(two);
     if (three) console.log(three);
 }
 foo(1, 2);
 
+/* Access arguments using indexer syntax*/
 function bar() {
     console.log("First argument value - " + arguments[0]);
 }
