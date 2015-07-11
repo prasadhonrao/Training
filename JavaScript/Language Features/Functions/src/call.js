bc255aaa257debc6377061b72dc1208216c5e051
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-07-11 11:39:40 +0530

    Updated

--- JavaScript/Language Features/Functions/src/call.js
+++ JavaScript/Language Features/Functions/src/call.js
@@ -9,3 +9,13 @@ var greet2 = function(name1, name2){
 	console.log(this.toUpperCase() + " "  + name1 + " and " + name2);
 };
 greet2.call("Hello", "Prasad" , "Rahul"); // passing "Hello" as context
+
+function foo() {
+	console.log(this.bar);
+}
+
+var bar = "bar1";
+var obj = {bar : "bar2"};
+
+foo();
+foo.call(obj);
