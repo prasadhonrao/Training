0ce57a3adf166929302cd7587e8d2680484d966f
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-07-23 14:05:53 +0530

    code refactored

--- JavaScript/Language Features/ES6/Variables and Parameters/rest-parameters.js
+++ JavaScript/Language Features/ES6/Variables and Parameters/rest-parameters.js
@@ -12,3 +12,29 @@ var doWork = function(name,...other) {
 
 doWork("Prasad");
 doWork("Prasad", "analysis", "development", "defect fixing");
+
+// -----------------------------------------------------------
+
+let sum = function() {
+  let result = 0;
+  for(let i=0; i< arguments.length; i++) {
+    result += arguments[i];
+  }
+  return result;
+};
+
+let result = sum(1,2,3);
+console.log(result);
+
+// Refactor using rest parameters
+// Always the last parameter in the function
+let sum2 = function(text, ...numbers) {
+  let result = 0;
+  for(let i=0; i< numbers.length; i++) {
+    result += numbers[i];
+  }
+  return result;
+};
+
+let result = sum2("Sum", 1,2,3);
+console.log(result);
--- JavaScript/Language Features/ES6/Variables and Parameters/spread.js
+++ JavaScript/Language Features/ES6/Variables and Parameters/spread.js
@@ -1,23 +1,14 @@
-let sum = function() {
-  let result = 0;
-  for(let i=0; i< arguments.length; i++) {
-    result += arguments[i];
-  }
-  return result;
-};
+function printNumbers(input1, input2) {
+  console.log(input1);
+  console.log(input2);
+}
 
-let result = sum(1,2,3);
-console.log(result);
+var numbers = [1, 2, 3, 4, 5];
+printNumbers(...numbers); // passing entire array
+printNumbers(numbers); // passing entire array will result into diff output
 
-// Refactor using rest parameters
-// Always the last parameter in the function
-let sum2 = function(text, ...numbers) {
-  let result = 0;
-  for(let i=0; i< numbers.length; i++) {
-    result += numbers[i];
-  }
-  return result;
-};
-
-let result = sum2("Sum", 1,2,3);
-console.log(result);
+// Creating array on the fly
+let first = [1, 2, 3, 4];
+let second = [5, 6, 7, 8];
+let mixed = [...first,...second];
+console.log(mixed);
