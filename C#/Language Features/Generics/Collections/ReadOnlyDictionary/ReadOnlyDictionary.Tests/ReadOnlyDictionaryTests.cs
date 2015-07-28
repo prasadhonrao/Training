edf479f88a5e53be4fcb51ca3d735de622d5d971
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-07-28 21:01:10 +0530

    ReadOnlyCollection examples added

---
+++ C#/Language Features/Generics/Collections/ReadOnlyDictionary/ReadOnlyDictionary.Tests/ReadOnlyDictionaryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReadOnlyDictionary.Tests
+{
+    [TestClass]
+    public class ReadOnlyDictionaryTests
+    {
+        [TestMethod]
+        public void ReadOnlyDictionary_Initialization_Test()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() { EIN = 1, FirstName = "John", LastName  = "Doe", Salary= 55000M },
+                new Employee() { EIN = 2, FirstName = "Jane", LastName = "Doe", Salary= 55000M },
+                new Employee() { EIN = 3, FirstName = "Don", LastName  = "DeLuth", Salary= 55000M },
+            };
+
+            Dictionary<int, Employee> einLookUp = employees.ToDictionary(x => x.EIN);
+            IReadOnlyDictionary<int, Employee> readOnlyStaff = einLookUp;
+            var eins = readOnlyStaff.Keys;
+            var allEmployees = readOnlyStaff.Values;
+            foreach (var emp in allEmployees)
+            {
+                Console.WriteLine(emp.FirstName + " " + emp.LastName);
+            }
+        }
+    }
+
+    class Employee
+    {
+        public int EIN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ C#/Language Features/Generics/Collections/ReadOnlyList/ReadOnlyList.Tests/ReadOnlyListTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReadOnlyList.Tests
+{
+    [TestClass]
+    public class ReadOnlyListTests
+    {
+        [TestMethod]
+        public void IReadOnlyList_Intialization()
+        {
+            IReadOnlyList<string> list = new[] { "Red", "Green", "Blue" };
+            Assert.IsTrue(list.Count == 3);
+        }
+    }
+}
