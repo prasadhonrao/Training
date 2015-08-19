8268fe1994c165980e0edd5a20b28565f483eeb8
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-08-19 10:54:13 +0530

    comparison operator example added

---
+++ JavaScript/Language Features/Crazy/crazy-comparison-operators.js
@@ -0,0 +1,7 @@
+// Evaluate one expression at a time and use Number function for coercion
+
+console.log(1 < 2 < 3); // true
+console.log(1 > 2 > 3); // false
+
+console.log(3 > 2 > 1); // ??
+console.log(3 < 2 < 1); // ??
