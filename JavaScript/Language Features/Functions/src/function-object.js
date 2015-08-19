4ebfac8738c8891b5ef702b47732584b97a2c913
Prasad Honrao <Honrao.Prasad@Gmail.com>
2015-08-19 14:02:07 +0530

    Code refactored

--- JavaScript/Language Features/Functions/src/function-object.js
+++ JavaScript/Language Features/Functions/src/function-object.js
@@ -2,7 +2,7 @@ function hello(msg) {
     console.log('Hello');
 };
 
-var paramLength = hello.length; // 
+var paramLength = hello.length;
 console.log("Number of parameters in function definition: " + paramLength);
 
 var functionName = hello.name;
@@ -10,3 +10,7 @@ console.log("function name: " + functionName);
 
 var functionbody = hello.toString();
 console.log("function body: " + functionbody);
+
+// Since functions are objects, you can add new members on the fly
+hello.language = 'english';
+console.log(hello.language);
