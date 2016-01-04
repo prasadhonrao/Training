24c3c372207b98a5cc0c0e93e6da99a6da5251ad
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-04 11:24:06 +0530

    Initial commit

---
+++ C#/Language Features/Asynchronous Programming/RSS Downloader/RSS Downloader/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSS_Downloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int counter = 0;
+        string channel9RSSUrl = "https://channel9.msdn.com/Feeds/RSS";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Synchronous Operation
+        //private void DownloadRSSButton_Click(object sender, RoutedEventArgs e)
+        //{
+            
+        //    var client = new WebClient();
+        //    var rss = client.DownloadString(channel9RSSUrl);
+        //    RSSTextBox.Text = rss;
+        //}
+        #endregion
+
+        #region Asynchronous Operation
+        private void DownloadRSSButton_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            client.DownloadStringCompleted += (s, args) =>
+            {
+                RSSTextBox.Text = args.Result;
+            };
+
+            client.DownloadStringAsync(new Uri(channel9RSSUrl));
+        }
+        #endregion
+
+        private void IncrementCounterButton_Click(object sender, RoutedEventArgs e)
+        {
+            IncrementOutputLabel.Content = counter++;
+        }
+    }
+}
