43567a8d4e944fd3a5c999a48533e5c9a0c44539
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-05 11:07:32 +0530

    OR operator example updated

--- JavaScript/Language Features/Operators/src/logical.js
+++ JavaScript/Language Features/Operators/src/logical.js
@@ -3,3 +3,16 @@ var first = true,
 
 console.log(first && second);
 console.log(first || second);
+
+/* 
+JavaScript OR operator ( || ) returns the first value in the expression 
+which can be coerced to true
+*/
+
+function greet(name) {
+	name = name || '<Default Value>';
+	console.log(name);
+}
+
+greet('Prasad');
+greet();
