af4a42d210ea03a72e61f20307bf6579ee6f8a10
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-09 09:08:22 +0530

    Example of declaring an object using Object.create added

--- JavaScript/Language Features/Objects/src/object-declaration.js
+++ JavaScript/Language Features/Objects/src/object-declaration.js
@@ -1,3 +1,4 @@
+// Object literal syntax
 var customer = {
 	firstName: 'Prasad',
 	lastName: 'Honrao',
@@ -6,9 +7,20 @@ var customer = {
 
 console.log(customer.firstName + " " + customer.lastName + " " + customer.gender)
 
+// Object constructor syntax
 var employee = new Object();
 employee.firstName = 'Prasad';
 employee.lastName = 'Honrao';
 employee.gender = 'M';
 
 console.log(employee.firstName + " " + employee.lastName + " " + employee.gender)
+
+// Object.create
+var dog = Object.create(Object.prototype, {
+	name: {value: 'tommy', enumerable: true, writable: true, configurable: true},
+	color: {value: 'white', enumerable: true, writable: true, configurable: true}
+})
+
+console.log(dog.name + " is a " + dog.color + " dog");
+
+// ES6 classes
