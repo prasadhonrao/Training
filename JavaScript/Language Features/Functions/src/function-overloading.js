5a5b8430a5c27308b5355d51a0ab577dfe888e0a
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-11 05:24:18 +0530

    Function overloading example enhanced

---
+++ JavaScript/Language Features/Functions/src/function-overloading.js
@@ -0,0 +1,39 @@
+/*
+	JavaScript does NOT support function overloading
+*/
+function add(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(1, 2));
+console.log(add(1, 2, 3));
+
+/*
+	Implement smaller functions and pass on default values 
+*/
+function greet(firstName, lastName, language) {
+	language = language || 'en';
+
+	if (language === 'en') {
+		message = 'Hello';
+	} else {
+		message = 'Hola';
+	}
+
+	console.log(message + ' ' + firstName + ' ' + lastName)
+}
+
+function greetEnglish(firstName, lastName) {
+	greet(firstName, lastName ,'en');
+}
+
+function greetSpanish(firstName, lastName) {
+	greet(firstName, lastName ,'es');
+}
+
+greetEnglish('Prasad', 'Honrao');
+greetSpanish('Colin', 'Malia');
