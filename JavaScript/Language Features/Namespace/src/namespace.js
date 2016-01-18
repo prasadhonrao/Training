c5416b960ec361d152b48aae28e68fd305265924
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-18 10:39:16 +0530

    Duplicate functions added in different namespaces to explain the namespace context

--- JavaScript/Language Features/Namespace/src/namespace.js
+++ JavaScript/Language Features/Namespace/src/namespace.js
@@ -1,15 +1,23 @@
 var namespace1 = namespace1 || {};
+
 namespace1.currentDate = function(){
 					var d = new Date();
 					return d.getDate();
 			};
 
 var namespace2 = namespace2 || {};
+
 namespace2.currentTime = function(){
 					var d = new Date();
 					return d.getTime();
 			}
 
-// console.log(currentDate()); //cant be accessed without namespace
+namespace2.currentDate = function(){
+					var d = new Date();
+					return d.getDate();
+			};
+
 console.log(namespace1.currentDate());
 console.log(namespace2.currentTime());
+console.log(namespace2.currentDate());
+//console.log(currentDate()); //cant be accessed without namespace
