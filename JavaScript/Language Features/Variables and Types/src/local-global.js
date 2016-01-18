0ffb0e75f609c80067237c91c4a61880da34fa3c
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-18 14:08:26 +0530

    Code refactored for global vs local variable

--- JavaScript/Language Features/Variables and Types/src/local-global.js
+++ JavaScript/Language Features/Variables and Types/src/local-global.js
@@ -1,9 +1,16 @@
-var message = "JavaScript World!"
+var message = "Global Variable"
 
 function displayMessage(){
-	var message = "Hello World"
+	var message = "Local Variable";
 	console.log(message);
 }
-console.log(message);
 
+function displayMessage2(){
+	message = "Local Variable 2"; // overridding global variable
+	console.log(message);
+}
+
+console.log(message);
 displayMessage();
+displayMessage2();
+console.log(message); // display overridden value instead of original global value
