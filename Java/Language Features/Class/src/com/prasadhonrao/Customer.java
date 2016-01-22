07bc54bec20bcb54680b4730fc8a5f8efd09bf5f
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-22 11:48:28 +0530

    Initial commit

---
+++ Java/Language Features/Class/src/com/prasadhonrao/Customer.java
@@ -0,0 +1,14 @@
+package com.prasadhonrao;
+
+/**
+ * Created by 130043 on 1/21/2016.
+ */
+public class Customer {
+    String name;
+    int age = 0;
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ Java/Language Features/Class/src/com/prasadhonrao/Main.java
@@ -0,0 +1,11 @@
+package com.prasadhonrao;
+import java.lang.String;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer newCustomer = new Customer("Prasad", 35);
+        System.out.println(newCustomer.name + " " + newCustomer.age);
+    }
+}
+
