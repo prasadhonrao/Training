5bd922327938aada5de1cb86cd7dfc04c6c5034c
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-23 17:35:40 +0530

    Initial commit

---
+++ Java/Language Features/Class Properties/src/com/prasadhonrao/classproperties/Main.java
@@ -0,0 +1,12 @@
+package com.prasadhonrao.classproperties;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Customer prasad = new Customer();
+        prasad.SetId(1);
+        prasad.SetName("Prasad");
+
+        System.out.println(prasad.GetCustomerInfo());
+    }
+}
