203895c6a6764c4edda92d1de044ad7cb03832af
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-25 15:46:59 +0530

    Initial commit

---
+++ Java/Language Features/Constructor/src/com/prasadhonrao/constructor/Main.java
@@ -0,0 +1,10 @@
+package com.prasadhonrao.constructor;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Customer prasad = new Customer(1);
+        Customer colin = new Customer("Colin");
+        Customer deepa = new Customer(3, "Deepa");
+    }
+}
