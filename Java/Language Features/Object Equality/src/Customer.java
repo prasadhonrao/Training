a5a3a15838f96883b75ba70478cd3465bb844df5
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-27 04:31:34 +0530

    Initial commit

---
+++ Java/Language Features/Object Equality/src/Customer.java
@@ -0,0 +1,7 @@
+public class Customer {
+	private int id;
+
+	public Customer(int id) {
+		this.id = id;
+	}
+}
---
+++ Java/Language Features/Object Equality/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		Customer c1 = new Customer(1);
+		Customer c2 = new Customer(1);
+		
+		System.out.println(c1 == c2); // reference equality check
+		System.out.println(c1.equals(c2)); // reference equality check
+
+		Person p1 = new Person(1);
+		Person p2 = new Person(1);
+		
+		System.out.println(p1 == p2); // reference equality check
+		System.out.println(p1.equals(p2)); // overridden equals
+	}
+}
