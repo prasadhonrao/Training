36b27f0ec406a68a1c77a57a0608362ac47686b7
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-27 14:54:49 +0530

    Property methods added

--- Java/Language Features/Workspace/Object Equality/src/Customer.java
+++ Java/Language Features/Workspace/Object Equality/src/Customer.java
@@ -2,6 +2,14 @@ public class Customer {
 	private int id;
 
 	public Customer(int id) {
+		this.setId(id);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
 		this.id = id;
 	}
 }
