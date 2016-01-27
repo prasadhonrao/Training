bb05434fbd0d6d976534b1686b16b039770ec3e2
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-27 16:27:20 +0530

    Initial commit

---
+++ Java/Language Features/Workspace/Exception Handling/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		int j = 2;
+		
+		try {
+			int result = i / (j-2);
+			System.out.println(result);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			System.out.println("exit...");
+		}
+	}
+
+}
