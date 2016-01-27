a588ee8622a4e269f8e7271b9dfd844b2ec53676
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-27 16:02:04 +0530

    Initial commit

---
+++ Java/Language Features/Workspace/Boxing - Unboxing/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Before Java 5 valueof and xxxValue pattern was compulsory
+		Integer i1 = Integer.valueOf(100); // boxing
+		int i2 = i1.intValue(); // unboxing
+		System.out.println(i1);
+		System.out.println(i2);
+		
+		// Java 5 and above infers the type based on the value
+		Integer a = 200; // boxing
+		int b = a; // unboxing
+		Integer c = b; // boxing
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
---
+++ Java/Language Features/Workspace/Primitive Types - Wrapper Classes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		Integer i = 100;
+		System.out.println(i.MIN_VALUE);
+		System.out.println(i.MAX_VALUE);
+		System.out.println(i.byteValue());
+		
+		Character ch = 'J';
+		System.out.println(ch.isLetter(ch));
+		System.out.println(ch.isDigit(ch));
+	}
+
+}
