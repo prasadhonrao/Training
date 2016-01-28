a79fda7536aaf67bf7ab3bbd40475521f207769d
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-28 14:35:50 +0530

    Initial commit

---
+++ Java/Language Features/Workspace/Package/src/Main.java
@@ -0,0 +1,10 @@
+import prasadhonrao.com.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer prasad = new Customer(1, "Prasad");
+		System.out.println(prasad.getName());
+	}
+
+}
---
+++ Java/Language Features/Workspace/Package/src/prasadhonrao/com/Customer.java
@@ -0,0 +1,27 @@
+package prasadhonrao.com;
+
+public class Customer {
+	private int id;
+	private String name;
+	
+	public Customer(int id, String name) {
+		this.setId(id);
+		this.setName(name);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
