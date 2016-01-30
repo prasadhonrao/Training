ee4ecc6ee5450b0fb42378c4af5dbae194c86be2
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-30 21:47:48 +0530

    Initial commit

---
+++ Java/Language Features/Workspace/Conditional Statements/src/Main.java
@@ -0,0 +1,48 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// if...else
+		
+		if (args.length > 0) {
+			System.out.println("You have provided command line argument : " + args[0]);
+		} else {
+			System.out.println("There are no command line argument");
+		}
+		
+		// switch...case
+		
+		int weekdayNumber = 7;
+		
+		switch(weekdayNumber) {
+			case 1:
+				System.out.println("Hello, Monday!");
+				break;
+			case 2:
+				System.out.println("Working Tuesday!");
+				break;
+			case 3:
+				System.out.println("Wednesday Meetings!");
+				break;
+			case 4:
+				System.out.println("Thursday Thunder");
+				break;
+			case 5:
+				System.out.println("Friday Weekend Near");
+				break;
+			case 6:
+				System.out.println("Saturday Night");
+				break;
+			case 7:
+				System.out.println("Happy Sunday!");
+				break;
+			default:
+				System.out.println("Invalid input");
+				break;
+		}
+		
+
+	}
+
+}
