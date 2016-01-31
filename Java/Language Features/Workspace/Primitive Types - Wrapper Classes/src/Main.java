864fdcee612d4612c1ae87b38fa6c74dd60e11c2
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-01-31 09:53:30 +0530

    Code corrected for static methods

--- Java/Language Features/Workspace/Primitive Types - Wrapper Classes/src/Main.java
+++ Java/Language Features/Workspace/Primitive Types - Wrapper Classes/src/Main.java
@@ -3,13 +3,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		Integer i = 100;
-		System.out.println(i.MIN_VALUE);
-		System.out.println(i.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
 		System.out.println(i.byteValue());
 		
 		Character ch = 'J';
-		System.out.println(ch.isLetter(ch));
-		System.out.println(ch.isDigit(ch));
+		System.out.println(Character.isLetter(ch));
+		System.out.println(Character.isDigit(ch));
 	}
 
 }
