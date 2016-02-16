68dfc66fc0ef2faf9d6d71bfa837bad8ab254720
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-02-16 14:40:17 +0530

    ES6 inheritance example updated

--- JavaScript/Language Features/Inheritance/ES6/es6-inheritance.js
+++ JavaScript/Language Features/Inheritance/ES6/es6-inheritance.js
@@ -12,11 +12,21 @@ class Person {
 // set the prototype using extends 
 
 class InformalPerson extends Person {
+	
 	constructor (firstName, lastName) {
 		super(firstName, lastName);
+	}
+
+	// hide base function if you want!
+	greet() {
+		return 'Yo ' + this.firstName;
 	}
 }
 
 var prasad = new InformalPerson('Prasad', 'Honrao');
 var greeting = prasad.greet();
 console.log(greeting);
+
+var jack = new Person('Jack', 'Taylor');
+var greeting2 = jack.greet();
+console.log(greeting2);
