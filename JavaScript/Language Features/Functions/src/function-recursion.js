8bfe9f6de5d39287c185f3bbe9362e14bb5db547
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-03-03 14:44:09 +0530

    function recursion example updated using arrow function

--- JavaScript/Language Features/Functions/src/function-recursion.js
+++ JavaScript/Language Features/Functions/src/function-recursion.js
@@ -1,3 +1,9 @@
+'use strict';
+
+/*
+Recursion - A function calls itself until it doesn't!
+*/
+
 function factorial(n) {
     if (n === 0 || n === 1) {
         return 1;
@@ -6,3 +12,15 @@ function factorial(n) {
 }
 
 console.log(factorial(5));
+
+console.log('---------------------------------');
+
+// Another example using ES 6 syntax
+
+let countdown = (num) => {
+	if (num === 0) return;
+	console.log(num);
+	countdown(num - 1);
+}
+
+countdown(10);
