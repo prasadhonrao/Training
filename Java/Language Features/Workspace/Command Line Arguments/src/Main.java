54d9fb5c94f1b454c2b1c5f7d12b6c73011a2b56
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-06-30 15:31:25 +0530

    Command line argument example added

---
+++ Java/Language Features/Workspace/Command Line Arguments/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			for (int i = 0; i < args.length ; i++) {
+				System.out.println(args[i]);		
+			}
+		}
+	}
+}
