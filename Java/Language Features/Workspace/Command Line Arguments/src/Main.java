5523771e5442abb67806fb7829b9174be2ed4de3
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-07 05:57:35 +0530

    Code refactored using for each loop

--- Java/Language Features/Workspace/Command Line Arguments/src/Main.java
+++ Java/Language Features/Workspace/Command Line Arguments/src/Main.java
@@ -1,9 +1,8 @@
-
 public class Main {
 	public static void main(String[] args) {
 		if (args.length > 0) {
-			for (int i = 0; i < args.length ; i++) {
-				System.out.println(args[i]);		
+			for (String item : args) {
+				System.out.println(item);		
 			}
 		}
 	}
