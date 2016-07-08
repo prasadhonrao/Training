04642dea812d7d4ba415a11c7f9cc5c57f079a50
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-08 07:47:03 +0530

    Initial commit
    Initial commit

---
+++ Java/Language Features/Workspace/Calculator/src/Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+	public int Add(int first, int second) {
+		return first + second;
+	}
+}
---
+++ Java/Language Features/Workspace/Reference Type/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		Flight flight1 = new Flight("E1234", 100, 150);
+		Flight flight2 = new Flight("KY7493", 20, 100);
+		
+		flight2.Add1Passenger();
+		System.out.println("Number of passengers in flight 1 = " + flight1.passengers);
+		System.out.println("Number of passengers in flight 2 = " + flight2.passengers);
+		
+		flight2 = flight1;
+		System.out.println("Number of passengers in flight 1 = " + flight1.passengers);
+		System.out.println("Number of passengers in flight 2 = " + flight2.passengers);
+		
+		flight2.Add1Passenger();
+		System.out.println("Number of passengers in flight 1 = " + flight1.passengers);
+		System.out.println("Number of passengers in flight 2 = " + flight2.passengers);
+	}
+}
