398fc9cc9a4985e44b51e19348435df7b3fb8d96
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-08 07:47:30 +0530

    Comment aligned
    Comment aligned

--- Java/Language Features/Workspace/Class - Method Overriding/src/CargoFlight.java
+++ Java/Language Features/Workspace/Class - Method Overriding/src/CargoFlight.java
@@ -1,5 +1,6 @@
 public class CargoFlight extends Flight {
-    // @Override declaration is options. This is just to explicitly indicate that this method overrides the base method
+    // @Override declaration is options. This is just to explicitly indicate 
+	// that this method overrides the base method
     @Override
     public int GetSeats(){
         return 12;
