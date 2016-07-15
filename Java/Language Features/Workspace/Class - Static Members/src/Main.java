51d4bd4d4f675d8c46613cc007bcea108eb9eedc
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-15 09:10:56 +0530

    Static members example added
    Static members example added

---
+++ Java/Language Features/Workspace/Class - Static Members/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	static String _serverName;
+	
+	static String GetServerName() {
+		return "Main Server";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Main.GetServerName());
+	}
+}
