5f133dabcf4a59f48bbbecf53f11326adffd1023
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-18 12:49:45 +0530

    Unit test example added
    Calculator class functions tested using JUnit

--- Java/Language Features/Workspace/Calculator/src/Calculator.java
+++ Java/Language Features/Workspace/Calculator/src/Calculator.java
@@ -2,4 +2,15 @@ public class Calculator {
 	public int Add(int first, int second) {
 		return first + second;
 	}
+	
+	public int Multiply(int first, int second) {
+		return first * second;
+	}
+	
+	public int Divide(int first, int second) throws ArithmeticException{
+		if (second == 0) {
+			throw new ArithmeticException("Cant divide by zero");
+		}
+		return first / second;
+	}
 }
--- Java/Language Features/Workspace/CalculatorTests/src/CalculatorTest.java
+++ Java/Language Features/Workspace/CalculatorTests/src/CalculatorTest.java
@@ -1,12 +1,53 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class CalculatorTest {
 
-	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	Calculator calc;
+	
+	@BeforeClass
+	public static void setUpClass(){
+		System.out.println("Before Class");
 	}
-
+	
+	@AfterClass
+	public static void tearDownClass(){
+		System.out.println("After Class");
+	}
+	
+	@Before
+	public void setUp() {
+		calc = new Calculator();
+		System.out.println("calc instance created");
+	}
+		
+	@Test
+	public void AddTest(){
+		int result = calc.Add(5, 5);
+		assertEquals(10, result);
+	}
+	
+	@Test
+	public void MultiplicationTest(){
+		int result = calc.Multiply(5, 5);
+		assertEquals(25, result);
+	}
+	
+	@Test
+	public void DivideTest(){
+		int result = calc.Divide(15, 5);
+		assertEquals(3, result);
+	}
+	
+	@Test(expected=ArithmeticException.class)
+	public void DivideByZeroTest(){
+		int result = calc.Divide(15, 0);
+		assertEquals(3, result);
+	}
+	
+	@After
+	public void tearDown() {
+		System.out.println("After!");
+	}
 }
