8d7bcaae56a67fe317b34014f551988a274b7023
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-23 09:55:16 +0530

    comment modified

--- JavaScript/Language Features/OOP/src/Abstraction/abstraction.js
+++ JavaScript/Language Features/OOP/src/Abstraction/abstraction.js
@@ -1,11 +1,10 @@
 /*
 Abstraction -  Information hiding is the principle of segregation of the design decisions in a computer program
-that are most likely to change, thus protecting other parts of the program from extensive modification 
+that are most likely to change, thus protecting other parts of the program from extensive modification
 if the design decision is changed.
 */
 function Barbarian(name){
-    // this variables are only available in the scope
-    // of this function.
+    // this variables are only available in the scope of this function.
     // Remember, in JavaScript, only functions create new scopes!
     var weapons = [],
         characterClass = "barbarian",
@@ -17,8 +16,7 @@ function Barbarian(name){
     };
     this.equipWeapon = function(weapon){
         weapon.equipped = true;
-        // Now we reference the weapons variable directly
-        // creating a closure.
+        // Now we reference the weapons variable directly creating a closure.
         // You cannot use this.weapons to access this variable any more
         // It is so private that the only way to access it is via closure
         weapons.push(weapon);
