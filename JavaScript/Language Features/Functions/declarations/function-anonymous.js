168f09850f3d639a068846dd46fa2fa0fd06f071
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-07-23 10:13:30 +0530

    code refactored with more examples

--- JavaScript/Language Features/Functions/declarations/function-anonymous.js
+++ JavaScript/Language Features/Functions/declarations/function-anonymous.js
@@ -1,19 +1,34 @@
 'use strict';
+
 // Function Declaration
 function callback() {
-	console.log("Done");
+	console.log("Callback got called!");
 }
-setTimeout(callback, 1000);
+callback();
+console.log("Function declaration name is " + callback.name);
 
 // Converted to anonymous function
-setTimeout(function () {
-	console.log("Done again!");
-}, 2000);
+var callback = function () {
+	console.log("Callback got called, again!");
+};
+callback();
+// Anonymous function doesn't have any name and 'callback' is just a variable referecen pointing to that function
+console.log("Anonymous function name is " + callback.name);
 
-// Always provide a way to anonymous function for 
+// Always provide a way to anonymous function for
 // 1. Better debugging support
 // 2. Function can call itself using recursion or unbind event handler etc.
 
 setTimeout(function callme() {
 	console.log("Thanks for naming me!");
 }, 3000);
+
+
+// Anonymous functions are useful to achieve recursion
+console.log("you can use recursion when an anonymous function is bound to a variable");
+var printSeries = function(n){
+    console.log('... SHOOOOOOOOOSH ....' + n);
+    if (n > 0)
+        printSeries(n-1);
+};
+printSeries(3);
--- JavaScript/Language Features/Functions/declarations/function-expression.js
+++ JavaScript/Language Features/Functions/declarations/function-expression.js
@@ -3,5 +3,6 @@ var squareFunc = function square (num) {
     return num * num;
 };
 
+console.log(squareFunc.name); //square
 console.log("2 Square : " + squareFunc(2));
 //console.log("2 Square : " + square(2)); //error
