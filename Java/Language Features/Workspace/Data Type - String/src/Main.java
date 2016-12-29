59ff02b3173b525dfe6d2090679414ce71ee29a4
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-12-29 06:29:41 +0530

    String methods added

--- Java/Language Features/Workspace/Data Type - String/src/Main.java
+++ Java/Language Features/Workspace/Data Type - String/src/Main.java
@@ -2,12 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
+
 		// String declaration
-		
 		String message = "Hello World!";
 		System.out.println(message);
+		
+		// String is an object so can be declared using constructor
+		String greeting = new String("Hello World 2!");
+		System.out.println(greeting);
 
-		// String equality
+		// String Methods
+		
+		// 1. String equality
 		String greeting1 = "I Love ";
 		greeting1 += "Java";
 		
@@ -16,7 +22,17 @@ public class Main {
 		System.out.println(greeting1 == greeting2); // Reference equality check
 		System.out.println(greeting1.equals(greeting2)); // character by character comparison
 		
+		// 2. Length
+		System.out.println("Number of characters in "  + message + " are " +  message.length());
 		
+		// 3. toUpperCase
+		System.out.println("hello world".toUpperCase());
+		
+		// 4. indexOf
+		System.out.println("hello world".indexOf('d'));
+		
+		// 5. replaceAll
+		System.out.println("hello world".replaceAll("hello", "hola"));
 	}
 
 }
