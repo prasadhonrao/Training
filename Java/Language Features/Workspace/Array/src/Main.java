7f384e9c40434e77a1b69a708dd87734546b334d
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-12-29 06:32:23 +0530

    Initial commit

---
+++ Java/Language Features/Workspace/Array/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[5];
+		
+		numbers[0] = 100;
+		System.out.println(numbers[0]);
+		System.out.println(numbers[1]); // int array elements gets initialized to zero by default
+		
+		// Iterating over array element
+		System.out.println("Array elements are - ");
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
