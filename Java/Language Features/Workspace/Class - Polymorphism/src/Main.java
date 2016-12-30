41738e5c8c119d944d71e22d4e7f760f5e2ac2f7
Prasad Honrao <Honrao.Prasad@Gmail.com>
2016-12-30 09:39:15 +0530

    Code refactored and comments added

--- Java/Language Features/Workspace/Class - Polymorphism/src/Main.java
+++ Java/Language Features/Workspace/Class - Polymorphism/src/Main.java
@@ -6,8 +6,9 @@ public class Main {
 		
 		Machine machine2 = new Printer();
 		machine2.turnOn();
-		//System.out.println(machine2 instanceof Machine );
-		
+		boolean isMachine2Printer = machine2 instanceof Machine; 
+		System.out.println("Is machine 2 as instance of Printer? " + isMachine2Printer );
+		 
 		Printer printer = new Printer();
 		printer.turnOn();
 	}
--- Java/Language Features/Workspace/Class - Polymorphism/src/Printer.java
+++ Java/Language Features/Workspace/Class - Polymorphism/src/Printer.java
@@ -1,4 +1,6 @@
 public class Printer extends Machine {
+	// @Override keyword is optional as all the methods in Java are virtual by default
+	@Override
 	public void turnOn() {
 		System.out.println("Printer is on");
 	}
