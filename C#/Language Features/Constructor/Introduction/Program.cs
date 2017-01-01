3fb32ef3f4ea8c4b065ca44cf075f7205df2a0d3
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-01 13:03:50 +0530

    Constructor example added

---
+++ C#/Language Features/Constructor/Introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var speaker1 = new Greeter("Prasad");
+            speaker1.SayHello("Jon");
+            Console.ReadLine();
+        }
+    }
+
+    class Greeter
+    {
+        private string speaker;
+        public Greeter(string speaker = "")
+        {
+            this.speaker = speaker;
+        }
+
+        public void SayHello(string recipient)
+        {
+            Console.WriteLine("Hello " + recipient + " From " + speaker);
+        }
+    }
+}
