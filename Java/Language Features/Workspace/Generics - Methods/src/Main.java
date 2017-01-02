83dd6efec9d4af2042b89014ca82c670609163b7
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-02 06:42:19 +0530

    Code warning added

--- Java/Language Features/Workspace/Generics - Methods/src/Main.java
+++ Java/Language Features/Workspace/Generics - Methods/src/Main.java
@@ -12,6 +12,10 @@ public class Main {
 		// Generic Method - Color printer is using BW cartridge
 		Printer<ColorCartridge> color2 = new Printer<ColorCartridge>(new ColorCartridge());
 		color2.printUsingCartridge(new BWCartridge());
+		
+		// Warning !! - Any type can be passed as generic type, so use generic constraints to avoid this
+		Printer<ColorCartridge> color3 = new Printer<ColorCartridge>(new ColorCartridge());
+		color3.printUsingCartridge(3);
 	}
 
 }
