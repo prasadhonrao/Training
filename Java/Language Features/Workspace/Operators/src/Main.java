9eba08c45a628c8a1869ce1de98492e15a6627f7
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-03 06:45:09 +0530

    Code refactored

--- Java/Language Features/Workspace/Operators/src/Main.java
+++ Java/Language Features/Workspace/Operators/src/Main.java
@@ -29,7 +29,36 @@ public class Main {
         System.out.println(firstNumber);
         System.out.println(secondNumber);
 
-        // LOGICAL
+        // LOGICAL OPERATORS
+        if (firstNumber > 10 & secondNumber > 10) {
+            System.out.println("Both the numbers are greater than 10");
+        } else {
+            System.out.println("One of the number is not greater than 10");
+        }
+
+        // OR
+        if (firstNumber > 10 | secondNumber > 10) {
+            System.out.println("Either of the number is greater than 10");
+        } else {
+            System.out.println("Both the numbers are not greater than 10");
+        }
+        
+        // EXCLUSIVE OR - Either of the value must be true or else expression will return false
+        boolean firstBool = true;
+        boolean secondBool = false;
+        boolean thridBool = true;
+        System.out.print("XOR : ");
+        System.out.println(firstBool ^ secondBool);
+        System.out.print("XOR : ");
+        System.out.println(firstBool ^ thridBool);
+
+        // NEGATION
+        boolean someValue = true;
+        boolean anotherValue = !someValue;
+        System.out.println(anotherValue);
+
+        // CONDITIONAL LOGICAL OPERATORS
+        // RHS executes only if LHS is true
         // AND
         if (firstNumber > 10 && secondNumber > 10) {
             System.out.println("Both the numbers are greater than 10");
@@ -44,18 +73,5 @@ public class Main {
             System.out.println("Both the numbers are not greater than 10");
         }
 
-        // EXCLUSIVE OR - Either of the value must be true or else expression will return false
-        boolean firstBool = true;
-        boolean secondBool = false;
-        boolean thridBool = true;
-        System.out.print("XOR : ");
-        System.out.println(firstBool ^ secondBool);
-        System.out.print("XOR : ");
-        System.out.println(firstBool ^ thridBool);
-
-        // NEGATION
-        boolean someValue = true;
-        boolean anotherValue = !someValue;
-        System.out.println(anotherValue);
     }
 }
