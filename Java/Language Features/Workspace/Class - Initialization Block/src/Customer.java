fedb793d07ee801d47a3d33cfae95830a3e4d954
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-04 06:28:02 +0530

    Initialization block example added

---
+++ Java/Language Features/Workspace/Class - Initialization Block/src/Customer.java
@@ -0,0 +1,22 @@
+
+public class Customer {
+	private int id;
+	
+	{
+		id = 100;
+	}
+	
+	public Customer() {
+		
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	
+}
---
+++ Java/Language Features/Workspace/Class - Initialization Block/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer cust = new Customer();
+		System.out.println(cust.getId());
+	}
+
+}
