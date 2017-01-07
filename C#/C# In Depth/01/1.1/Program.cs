ef7315e11f6094d73569fef85fff18b9544ed471
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-07 13:46:49 +0530

    Initial commit

---
+++ C#/C# In Depth/01/1.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+    class Product
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        decimal price;
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+        }
+
+        public Product(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public static ArrayList GetSampleProducts()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(new Product("West Side Story", 9.99m));
+            list.Add(new Product("Assassins", 14.99m));
+            list.Add(new Product("Frogs", 13.99m));
+            list.Add(new Product("Sweeny Todd", 10.99m));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", name, price);
+        }
+    }
+}
