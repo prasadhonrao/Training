a0d90c5f9a5ffcfd48657dfed1b85957d9db1779
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-08 06:34:04 +0530

    1.2, 1.3 code added

---
+++ C#/C# In Depth/01/1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    class Product
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        decimal price;
+        public decimal Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public static ArrayList GetSampleProducts()
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("West Side Story", 9.99m));
+            list.Add(new Product("Assassins", 14.99m));
+            list.Add(new Product("Frogs", 13.99m));
+            list.Add(new Product("Sweeny Todd", 10.99m));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", name, price);
+        }
+    }
+}
