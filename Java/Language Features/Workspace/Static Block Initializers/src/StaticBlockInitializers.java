c592fceec4c672377ac04eae6cebb33d1e6fd5cc
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-08 11:58:42 +0530

    Static block initializers example added

---
+++ Java/Language Features/Workspace/Static Block Initializers/src/StaticBlockInitializers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class StaticBlockInitializers {
+
+    public static int B = 0;
+    public static int H = 0;
+    public static boolean flag = false;
+    
+	static {
+	    Scanner scan = new Scanner(System.in);
+	    B = scan.nextInt();
+	    H = scan.nextInt();
+	    scan.close();
+	    
+	    if (B > 0 && H > 0) {
+	        flag = true;
+	    } else {
+	        System.out.println("java.lang.Exception: Breadth and height must be positive");
+	    }
+	}
+	
+	public static void main(String[] args) {
+		
+		if (flag) {
+			int area = B*H;
+			System.out.println(area);
+		}
+		
+	}
+
+}
