6c9ad52ed8793f9b8e02caf687665180c583eae2
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-09 05:43:31 +0530

    File handling related exception added

---
+++ Java/Language Features/Workspace/Exception Handling/src/ExceptionHandling.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+		HandleDivideByZeroException();
+		HandleFileException();
+	}
+	
+	public static void HandleDivideByZeroException() {
+		int i = 10;
+		int j = 2;
+		
+		try {
+			int result = i / (j-2);
+			System.out.println(result);
+		} catch (ArithmeticException e) {
+			System.out.println("Invalid Value : Message : " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			//System.out.println("exit...");
+		}
+	}
+
+	public static void HandleFileException() {
+		
+		BufferedReader reader = null;
+		int total = 0;
+		String line = null;
+		String filePath;
+		
+		try {
+			filePath = new File("numbers.txt").getAbsolutePath();
+			reader = new BufferedReader(new FileReader(filePath));
+		
+			while((line = reader.readLine()) != null) {
+				total += Integer.valueOf(line);
+			}
+
+			System.out.println("Total is :" + total);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (reader != null){
+					reader.close();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
