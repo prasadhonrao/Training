55e5b6b7364bf1221e52745673042dd4ac4f1ae3
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-10 11:55:08 +0530

    C# in depth code 1.6, 1.7, 1.8 added

---
+++ C#/C# In Depth/01/1.7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Product> products = Product.GetSampleProducts();
+
+        products.Sort(delegate (Product x, Product y) {
+            return x.Name.CompareTo(y.Name);
+        });
+
+        foreach (Product product in products)
+        {
+            Console.WriteLine(product);
+        }
+    }
+    class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        Product() { }
+
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product>()
+            {
+                new Product("West Side Story", 9.99m),
+                new Product("Assassins", 14.99m),
+                new Product("Frogs", 13.99m),
+                new Product("Sweeny Todd", 10.99m)
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Name, Price);
+        }
+    }
+}
