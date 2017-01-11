d9eec91020ead061701857963eef49b96c009420
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-11 13:02:23 +0530

    Annotation example added

---
+++ Java/Language Features/Workspace/Annotations/src/Annotations.java
@@ -0,0 +1,10 @@
+public class Annotations {
+	public static void main(String[] args) {
+		deprecatedMethod();
+	}
+	
+	@Deprecated
+	static void deprecatedMethod() {
+		System.out.println("This is deprecated method");
+	}
+}
