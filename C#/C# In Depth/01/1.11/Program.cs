ad7734f9d00f8a038bf70904a38b88d602263667
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-12 07:05:41 +0530

    Code 1.9, 1.10, 1.11 added

---
+++ C#/C# In Depth/01/1.11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Product> products = Product.GetSampleProducts();
+        Predicate<Product> test = delegate (Product p) { return p.Price > 10m; };
+        List<Product> matches = products.FindAll(test);
+
+        Action<Product> print = Console.WriteLine;
+        matches.ForEach(print);
+    }
+
+    class Product
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        decimal price;
+        public decimal Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public static List<Product> GetSampleProducts()
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("West Side Story", 9.99m));
+            list.Add(new Product("Assassins", 14.99m));
+            list.Add(new Product("Frogs", 13.99m));
+            list.Add(new Product("Sweeny Todd", 10.99m));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", name, price);
+        }
+    }
+}
---
+++ C#/C# In Depth/01/1.9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Product> products = Product.GetSampleProducts();
+        
+        foreach (Product product in products.OrderBy(p => p.Name))
+        {
+            Console.WriteLine(product);
+        }
+    }
+    class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        Product() { }
+
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product>()
+            {
+                new Product("West Side Story", 9.99m),
+                new Product("Assassins", 14.99m),
+                new Product("Frogs", 13.99m),
+                new Product("Sweeny Todd", 10.99m)
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Name, Price);
+        }
+    }
+}
