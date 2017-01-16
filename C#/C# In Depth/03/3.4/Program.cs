2f692607bc290ac4b662ab8ec266de3b2d739d52
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-16 10:26:07 +0530

    3.4 code added

---
+++ C#/C# In Depth/03/3.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompareToDefault("x"));
+            Console.WriteLine(CompareToDefault(10));
+            Console.WriteLine(CompareToDefault(0));
+            Console.WriteLine(CompareToDefault(-10));
+            Console.WriteLine(CompareToDefault(DateTime.MinValue));
+
+
+            Console.WriteLine("*******************");
+            Console.WriteLine(CompareToDefaultUsingIEquatable("x"));
+            Console.WriteLine(CompareToDefaultUsingIEquatable(10));
+            Console.WriteLine(CompareToDefaultUsingIEquatable(0));
+            Console.WriteLine(CompareToDefaultUsingIEquatable(-10));
+            Console.WriteLine(CompareToDefaultUsingIEquatable(DateTime.MinValue));
+
+            Console.ReadLine();
+        }
+
+        static int CompareToDefault<T>(T value) where T : IComparable<T>
+        {
+            return value.CompareTo(default(T));
+        }
+
+        static bool CompareToDefaultUsingIEquatable<T>(T value) where T : IEquatable<T>
+        {
+            return value.Equals(default(T));
+        }
+    }
+}
