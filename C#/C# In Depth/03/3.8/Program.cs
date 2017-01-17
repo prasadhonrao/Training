dc85dd35e6c73e544422e4f605924f8e076a0902
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-17 09:43:28 +0530

    3.8 code added

---
+++ C#/C# In Depth/03/3.8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeWithField<int>.field = "First";
+            TypeWithField<string>.field = "Second";
+            TypeWithField<DateTime>.field = "Third";
+
+            TypeWithField<int>.PrintField();
+            TypeWithField<string>.PrintField();
+            TypeWithField<DateTime>.PrintField();
+
+        }
+    }
+
+    class TypeWithField<T>
+    {
+        public static string field;
+        public static void PrintField()
+        {
+            Console.WriteLine(field + ": " + typeof(T).Name);
+        }
+    }
+}
