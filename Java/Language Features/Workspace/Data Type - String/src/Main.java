0ed86854aff8cecc36754277471bda1407d32b26
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-17 14:45:38 +0530

    Code reformatted

--- Java/Language Features/Workspace/Data Type - String/src/Main.java
+++ Java/Language Features/Workspace/Data Type - String/src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -6,41 +5,46 @@ public class Main {
 		// String declaration
 		String message = "Hello World!";
 		System.out.println(message);
-		
+
 		// String is an object so can be declared using constructor
 		String greeting = new String("Hello World 2!");
 		System.out.println(greeting);
-		
-		// Strings are immutable 
+
+		// Strings are immutable
 		String user = "Prasad";
-		user = user + " Honrao";  // creates a new string (immutable)
+		user = user + " Honrao"; // creates a new string (immutable)
 		System.out.println(user);
 
 		// String Methods
-		
+
 		// 1. String equality
 		String greeting1 = "I Love ";
 		greeting1 += "Java";
-		
+
 		String greeting2 = "I Love Java";
-		
+
 		System.out.println(greeting1 == greeting2); // Reference equality check
-		System.out.println(greeting1.equals(greeting2)); // character by character comparison
-		
+		System.out.println(greeting1.equals(greeting2)); // character by
+															// character
+															// comparison
+
 		String greeting3 = greeting1.intern();
 		String greeting4 = greeting2.intern();
 		System.out.print("Intern : ");
-		System.out.println(greeting3 == greeting4); // Useful for lot of string comparison, inexpensive as compared to character by character comparison 
-		
+		System.out.println(greeting3 == greeting4); // Useful for lot of string
+													// comparison, inexpensive
+													// as compared to character
+													// by character comparison
+
 		// 2. Length
-		System.out.println("Number of characters in "  + message + " are " +  message.length());
-		
+		System.out.println("Number of characters in " + message + " are " + message.length());
+
 		// 3. toUpperCase
 		System.out.println("hello world".toUpperCase());
-		
+
 		// 4. indexOf
 		System.out.println("hello world".indexOf('d'));
-		
+
 		// 5. replaceAll
 		System.out.println("hello world".replaceAll("hello", "hola"));
 	}
--- Java/Language Features/Workspace/Primitive Types/src/Main.java
+++ Java/Language Features/Workspace/Primitive Types/src/Main.java
@@ -1,53 +1,54 @@
 public class Main {
 
-    public static void main(String[] args) {
-        // 1. INTEGER TYPES - byte (8), short (16), int (32), long (64)
-    	System.out.println("Whole Number / Integer Types");
-        byte byteValue = 127;
-        System.out.println(byteValue);
-        System.out.println("Byte Min Value : " + Byte.MIN_VALUE);
-        System.out.println("Byte Max Value : " + Byte.MAX_VALUE);
+	public static void main(String[] args) {
+		// 1. INTEGER TYPES - byte (8), short (16), int (32), long (64)
+		System.out.println("Whole Number / Integer Types");
+		byte byteValue = 127;
+		System.out.println(byteValue);
+		System.out.println("Byte Min Value : " + Byte.MIN_VALUE);
+		System.out.println("Byte Max Value : " + Byte.MAX_VALUE);
 
-        short shortValue = 32767;
-        System.out.println(shortValue);
-        System.out.println("Short Min Value : " + Short.MIN_VALUE);
-        System.out.println("Short Max Value : " + Short.MAX_VALUE);
+		short shortValue = 32767;
+		System.out.println(shortValue);
+		System.out.println("Short Min Value : " + Short.MIN_VALUE);
+		System.out.println("Short Max Value : " + Short.MAX_VALUE);
 
-        int intValue = 1000;
-        System.out.println(intValue);
-        System.out.println("Integer Min Value : " + Integer.MIN_VALUE);
-        System.out.println("Integer Max Value : " + Integer.MAX_VALUE);
+		int intValue = 1000;
+		System.out.println(intValue);
+		System.out.println("Integer Min Value : " + Integer.MIN_VALUE);
+		System.out.println("Integer Max Value : " + Integer.MAX_VALUE);
 
-        long longValue = 22222235654677L; // Note L at the end which denotes long value
-        System.out.println(longValue);
-        System.out.println("Long Min Value : " + Long.MIN_VALUE);
-        System.out.println("Long Max Value : " + Long.MAX_VALUE);
+		long longValue = 22222235654677L; // Note L at the end which denotes
+											// long value
+		System.out.println(longValue);
+		System.out.println("Long Min Value : " + Long.MIN_VALUE);
+		System.out.println("Long Max Value : " + Long.MAX_VALUE);
 
-        // 2. FLOATING POINT TYPE - float (32), double (64)
-        System.out.println("\nFloating Point Types");
-        float pi = 1.345f; // Note f at the end
-        System.out.println(pi);
-        System.out.println("Float Min Value : " + Float.MIN_VALUE);
-        System.out.println("Float Max Value : " + Float.MAX_VALUE);
+		// 2. FLOATING POINT TYPE - float (32), double (64)
+		System.out.println("\nFloating Point Types");
+		float pi = 1.345f; // Note f at the end
+		System.out.println(pi);
+		System.out.println("Float Min Value : " + Float.MIN_VALUE);
+		System.out.println("Float Max Value : " + Float.MAX_VALUE);
 
-        double newPi = 1.34566778d; // Note d at the end
-        System.out.println(newPi);
-        System.out.println("Double Min Value : " + Double.MIN_VALUE);
-        System.out.println("Double Max Value : " + Double.MAX_VALUE);
+		double newPi = 1.34566778d; // Note d at the end
+		System.out.println(newPi);
+		System.out.println("Double Min Value : " + Double.MIN_VALUE);
+		System.out.println("Double Max Value : " + Double.MAX_VALUE);
 
-        // 3. CHARACTER - char
-        System.out.println("\nCharacter Type");
-        char charValue = 'c';
-        System.out.println(charValue);
-        System.out.println("Character Min Value : " + Character.MIN_VALUE);
-        System.out.println("Character Max Value : " + Character.MAX_VALUE);
+		// 3. CHARACTER - char
+		System.out.println("\nCharacter Type");
+		char charValue = 'c';
+		System.out.println(charValue);
+		System.out.println("Character Min Value : " + Character.MIN_VALUE);
+		System.out.println("Character Max Value : " + Character.MAX_VALUE);
 
-        char accentedU = '\u00DA'; 
-        System.out.println("Unicode Accented U : " + accentedU);
+		char accentedU = '\u00DA';
+		System.out.println("Unicode Accented U : " + accentedU);
 
-        // 4. BOOLEAN
-        System.out.println("\nBoolean Type");
-        boolean iLoveJava = true;
-        System.out.println(iLoveJava);
-    }
+		// 4. BOOLEAN
+		System.out.println("\nBoolean Type");
+		boolean iLoveJava = true;
+		System.out.println(iLoveJava);
+	}
 }
