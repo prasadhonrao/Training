7fc70249f3718006f1a2915bfd7ce7e11afd8169
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-18 15:57:28 +0530

    Comment reformatted

--- Java/Language Features/Workspace/Array/src/Array.java
+++ Java/Language Features/Workspace/Array/src/Array.java
@@ -2,11 +2,13 @@ public class Array {
 
 	public static void main(String[] args) {
 		int[] numbers = new int[5];
-		
+
 		numbers[0] = 100;
 		System.out.println(numbers[0]);
-		System.out.println(numbers[1]); // int array elements gets initialized to zero by default
-		
+
+		// int array elements gets initialized to zero by default
+		System.out.println(numbers[1]);
+
 		// Iterating over array element
 		System.out.println("Array elements are - ");
 		for (int i = 0; i < numbers.length; i++) {
