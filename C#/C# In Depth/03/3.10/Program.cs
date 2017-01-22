3ab0fc9e2fb3a7b68cfffda76792709b798d95f6
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-22 12:50:04 +0530

    3.10 code added

---
+++ C#/C# In Depth/03/3.10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountingEnumerable counter = new CountingEnumerable();
+            foreach (var item in counter)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class CountingEnumerable : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new CountingEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    internal class CountingEnumerator : IEnumerator<int>
+    {
+        int current = -1;
+        public bool MoveNext()
+        {
+            current++;
+            return current < 10;
+        }
+
+        public int Current { get { return current; } }
+
+        object IEnumerator.Current { get { return Current; } }
+
+        public void Reset()
+        {
+            current = -1;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("All the values are printed now!");
+        }
+    }
+}
