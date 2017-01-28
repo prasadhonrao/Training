3a4af38d2abbacf15f117f6bfeb369f38135c41d
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-28 06:03:12 +0530

    Removed unused using statements

--- C#/Language Features/Delegates/Custom Multicast Delegates/WorkType.cs
+++ C#/Language Features/Delegates/Custom Multicast Delegates/WorkType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Training.CSharp
+﻿namespace Training.CSharp
 {
     public enum WorkType
     {
