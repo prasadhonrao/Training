965234e39a857499adc9b2c435e0bedea9239197
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-30 15:15:16 +0530

    Remove unused using statements

--- C#/Language Features/Mutable-Immutable/Mutable-Immutable/Mutable.cs
+++ C#/Language Features/Mutable-Immutable/Mutable-Immutable/Mutable.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mutable_Immutable
+﻿namespace Mutable_Immutable
 {
-    public class Mutable
+    public sealed class Mutable
     {
         public int Value { get; set; }
     }
