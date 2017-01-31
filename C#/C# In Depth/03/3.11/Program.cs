5d999a21a055bef92173abd96e1a4b80926bb4ff
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-01-31 13:53:19 +0530

    Add 3.11 code

---
+++ C#/C# In Depth/03/3.11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemonstrateTypeof<int>();
+
+            Console.ReadLine();
+        }
+
+        static void DemonstrateTypeof<X>()
+        {
+            Console.WriteLine(typeof(X));
+            Console.WriteLine(typeof(List<>));
+            Console.WriteLine(typeof(Dictionary<,>));
+            Console.WriteLine(typeof(List<X>));
+            Console.WriteLine(typeof(Dictionary<string, X>));
+            Console.WriteLine(typeof(List<long>));
+            Console.WriteLine(typeof(List<DateTime>));
+            Console.WriteLine(typeof(Dictionary<long, Guid>));
+            Console.WriteLine(typeof(Tuple<,,,>));
+            Console.WriteLine(typeof(Tuple<X, X>));
+            Console.WriteLine(typeof(SortedDictionary<X, X>));
+        }
+    }
+}
