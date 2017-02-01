4504f7dade78efe2dcf1c16d9566300d1aad97ee
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-02-01 15:41:28 +0530

    Add 4.1

---
+++ C#/C# In Depth/04/4.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> x = new Nullable<int>(5);
+            Console.WriteLine("Instance with value");
+            Display(x);
+
+            x = new Nullable<int>();
+            Console.WriteLine("Instance without value");
+            Display(x);
+            Console.ReadLine();
+
+        }
+
+        static void Display(Nullable<int> x)
+        {
+            Console.WriteLine("HasValue : {0}", x.HasValue);
+            if (x.HasValue)
+            {
+                Console.WriteLine("Value : {0}", x.Value);
+                Console.WriteLine("Explicit Conversion: {0}", (int)x);
+            }
+            Console.WriteLine("GetValueOrDefault() : {0}", x.GetValueOrDefault());
+            Console.WriteLine("GetValueOrDefault(10) : {0}", x.GetValueOrDefault(10));
+            Console.WriteLine("ToString() : \"{0}\"", x.ToString());
+            Console.WriteLine("GetHashCode() : {0}", x.GetHashCode());
+            Console.WriteLine();
+        }
+    }
+}
