c3cd4263a22e54025fe2130f8a7c28f7fb697252
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-02-04 09:28:55 +0530

    Add 4.3

---
+++ C#/C# In Depth/04/4.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nullable<int> nullable = 5;
+        Object boxed = nullable;
+        Console.WriteLine(boxed.GetType());
+        
+        int normal = (int)boxed;
+        Console.WriteLine(normal);
+        
+        nullable = (Nullable<int>)boxed;
+        Console.WriteLine(nullable);
+        
+        nullable = new Nullable<int>();
+        boxed = nullable;
+        
+        Console.WriteLine(boxed == null);
+        
+        nullable = (Nullable<int>)boxed;
+        Console.WriteLine(nullable.HasValue);
+    }
+}
