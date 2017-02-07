2ad2850e83ab82212fd08d29b43718734a6243a1
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-02-07 14:28:36 +0530

    Add 4.5

---
+++ C#/C# In Depth/04/4.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class MainClass {
+  public static void Main (string[] args) {
+    int? parsed = TryParse("Not valid");
+    if (parsed != null)
+    {
+      Console.WriteLine("Parsed to {0}", parsed.Value);
+    }
+    else
+    {
+      Console.WriteLine("Could not parse");
+    }
+  }
+  
+  static int TryParse(string text)
+  {
+    int ret;
+    if (int.TryParse(text, out ret))
+    {
+      return ret;
+    }
+    else
+    {
+      return null;
+    }
+  }
+}
