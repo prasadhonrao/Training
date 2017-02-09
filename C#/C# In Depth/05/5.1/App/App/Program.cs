7b7cbb18b632b4692caf8fb553c208dc29396e94
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-02-09 16:08:58 +0530

    Add 5.1

---
+++ C#/C# In Depth/05/5.1/App/App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Button b = new Button();
+            b.Text = "Click Me";
+            b.Click += new EventHandler(LogPlainEvent);
+            b.KeyPress += new KeyPressEventHandler(LogKeyEvent);
+            b.MouseClick += new MouseEventHandler(LogMouseEvent);
+
+        }
+
+        static void LogPlainEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("LogPlain");
+        }
+        static void LogKeyEvent(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("LogKey");
+        }
+
+        static void LogMouseEvent(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("LogMouse");
+        }
+
+
+    }
+}
