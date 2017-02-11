9d413c6d867713aa754be3109fa6d88211d88035
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-02-11 07:21:01 +0530

    Refactor code

--- C#/Language Features/Task Parallel Library/Sequential vs Parallel Code/Program.cs
+++ C#/Language Features/Task Parallel Library/Sequential vs Parallel Code/Program.cs
@@ -1,58 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Sequential_vs_Parallel_Code
 {
     class Program
     {
+        const int ITERATIONS = 5;
         static void Main(string[] args)
         {
-            //RunSequential();
+            RunSequential();
+            Console.WriteLine();
             RunParallel();
 
             Console.ReadLine();
         }
+        private static void RunSequential()
+        {
+            Console.WriteLine("Sequential execution start...");
+            PrintFirst();
+            PrintSecond();
+            PrintThird();
+            Console.WriteLine("Sequential execution end...");
+        }
 
         private static void RunParallel()
         {
+            Console.WriteLine("Parallel execution start...");
             Task.Factory.StartNew(() =>
             {
                 PrintFirst();
                 PrintSecond();
                 PrintThird();
             });
+            Console.WriteLine("Parallel execution end...");
         }
 
-        private static void RunSequential()
-        {
-            PrintFirst();
-            PrintSecond();
-            PrintThird();
-        }
 
         static void PrintFirst()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < ITERATIONS; i++)
             {
                 Console.Write("\t First");
             }
+            Console.WriteLine();
         }
         static void PrintSecond()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < ITERATIONS; i++)
             {
                 Console.Write("\t Second");
             }
+            Console.WriteLine();
         }
         static void PrintThird()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < ITERATIONS; i++)
             {
                 Console.Write("\t Third");
             }
+            Console.WriteLine();
         }
     }
 
