a91318062c8ed598a37cc2191bf502ef6d652210
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-02-28 12:38:02 +0530

    Refactor code

--- C#/Language Features/LINQ/Query OData/Program.cs
+++ C#/Language Features/LINQ/Query OData/Program.cs
@@ -11,35 +11,48 @@ namespace Training.CSharp
             var db = new NorthwindEntities(new Uri("http://services.odata.org/Northwind/Northwind.svc"));
 
             #region 1. Display Categories
-            var categories = db.Categories;
 
             Console.WriteLine("Categories");
-            foreach (var category in categories)
-            {
-                Console.WriteLine(category.CategoryName);
-            }
+            
+            //var categories = db.Categories;
+            //foreach (var category in categories)
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+
+            db.Categories.ToList().ForEach(c => Console.WriteLine(c.CategoryName));
 
             Console.WriteLine();
             #endregion
 
             #region 2. Get Customers in London
+
             Console.WriteLine("Customers in London");
-            var CustomersInLondon = db.Customers.Where(c => c.City == "London");
-            foreach (var customer in CustomersInLondon)
-            {
-                Console.WriteLine(customer.ContactName);
-            }
+            //var CustomersInLondon = db.Customers.Where(c => c.City == "London");
+            //foreach (var customer in CustomersInLondon)
+            //{
+            //    Console.WriteLine(customer.ContactName);
+            //}
+
+            db.Customers.Where(c => c.City == "London").ToList().ForEach(c => Console.WriteLine(c.ContactName));
             Console.WriteLine();
+
+
             #endregion
 
             #region 3. Call View / SP
 
             Console.WriteLine("Alphabetical list of products");
-            var products = db.Alphabetical_list_of_products.OrderBy(p => p.ProductName); // Alphabetical_list_of_products service does not return in products in alphabetical order!
-            foreach (var product in products)
-            {
-                Console.WriteLine(product.ProductName);
-            }
+
+            // Alphabetical_list_of_products service does not return in products in alphabetical order!
+            //var products = db.Alphabetical_list_of_products.OrderBy(p => p.ProductName); 
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            db.Alphabetical_list_of_products.OrderBy(p => p.ProductName).ToList().ForEach(p => Console.WriteLine(p.ProductName));
+
             Console.WriteLine();
 
             #endregion
