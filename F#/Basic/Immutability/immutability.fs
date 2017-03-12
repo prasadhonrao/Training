4f549a68cd7ab8b3c3eb918a3291c348a708a334
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-12 08:50:53 +0530

    Add immutability example

---
+++ F#/Basic/Immutability/immutability.fs
@@ -0,0 +1,12 @@
+let numbers = [3;2;1]
+let moreNumbers = 4 :: numbers // prepend 4 with numbers
+
+printfn "numbers..."
+
+for i in numbers do
+   printfn "%d" i
+
+printfn "more numbers..."
+
+for i in moreNumbers do
+   printfn "%d" i
