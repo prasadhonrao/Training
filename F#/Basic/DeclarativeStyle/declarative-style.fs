d5253a674cbf655ed630792c736d387c1a48521a
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-13 08:01:01 +0530

    declarative style examples added

--- F#/Basic/DeclarativeStyle/declarative-style.fs
+++ F#/Basic/DeclarativeStyle/declarative-style.fs
@@ -10,3 +10,13 @@ let square2 (x:float) : float = x * x
 let sqaured2 = List.map square2 [1.2;2.5;3.9]
 for j in sqaured2 do
    printfn "%f" j
+
+// doubling list values using map function
+let doubles = List.map((*) 2) [1;2;3;4]
+for k in doubles do
+    printfn "%d" k
+
+// add squares
+let addsquares a b = (a*a) + (b*b)
+let result = addsquares 5 5
+printfn "%d" result
