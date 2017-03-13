81b692b008643b4791c1e8a369b1369671c3d7c1
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-13 08:01:19 +0530

    string example added

--- F#/Basic/Type/type.fs
+++ F#/Basic/Type/type.fs
@@ -1,3 +1,20 @@
+// string
+let hello = "Hello" + " World"
+printfn "%s" hello
+
+let verbatimXml = @"<book title=""Paradise Lost"">"
+printfn "%s" verbatimXml
+
+let tripleXml = """<book title="Paradise Lost">"""
+printfn "%s" tripleXml
+
+let poem =
+    "The lesser world was daubed\n\
+     By a colorist of modest skill\n\
+     A master limned you in the finest inks\n\
+     And with a fresh-cut quill."
+printfn "%s" poem
+
 type TwoNumbers = int * int // a type with two integer values
 
 let add((first, second):TwoNumbers) =
