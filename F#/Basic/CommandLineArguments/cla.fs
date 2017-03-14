1a20c22d833652adda06617f10dcdaf4897bff7a
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-14 12:10:32 +0530

    Add code for user inputs

--- F#/Basic/CommandLineArguments/cla.fs
+++ F#/Basic/CommandLineArguments/cla.fs
@@ -1,4 +1,14 @@
+open System
+
+// [<EntryPoint>]
+// let main argv =
+//     printfn "%A" argv
+//     0 // return an integer exit code
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+    let a = Console.ReadLine() |> int
+    let b = Console.ReadLine() |> int
+    printfn "%d" (a+b)
     0 // return an integer exit code
