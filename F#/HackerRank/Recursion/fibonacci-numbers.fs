fcb90ada18c61730819e2f9909ef4317219b3a11
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-15 05:07:06 +0530

    Add fibonacci numbers sol

---
+++ F#/HackerRank/Recursion/fibonacci-numbers.fs
@@ -0,0 +1,9 @@
+open System
+
+let rec fib n =
+    match n with
+    | x when x = 1 -> 0
+    | x when x = 2 -> 1
+    | _ -> fib (n - 2) + fib (n - 1)
+
+printfn "%d" (fib (Console.ReadLine() |> int) |>int)
