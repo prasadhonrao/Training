6041d61c0b3b772d0d294712442aeb5f86258dc5
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-14 17:02:19 +0530

    list replication added

---
+++ F#/HackerRank/Introduction/list-replication.fs
@@ -0,0 +1,13 @@
+open System
+
+let n = Console.ReadLine() |> int
+let list =
+    [
+     let mutable key = Console.ReadLine()
+     while not (key = null) do
+        yield key
+        key <- Console.ReadLine()
+    ]
+
+list
+|> List.iter (fun i -> ( for j = 1 to n do printfn "%s" i))
