7b856f8496895fa219665aa3e8b1b313231a226b
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-15 12:25:07 +0530

    Add extension method

---
+++ F#/Basic/Extensions/extensions.fs
@@ -0,0 +1,10 @@
+type System.String with
+    member x.IsCapitalized
+        with get() =
+            Char.IsUpper(x.[0])
+
+let name = "F# is really cool language"
+
+printfn "Name capitalized? %b" name.IsCapitalized
+
+printfn "%b" "i am not captialized".IsCapitalized
