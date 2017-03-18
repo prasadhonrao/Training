1fafe9d94fb5fc05ba50632bf460de4cdaac5307
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-18 16:32:57 +0530

    Add array method

--- F#/Collections/Array/array.fs
+++ F#/Collections/Array/array.fs
@@ -26,3 +26,12 @@ printfn "%A" first100
 
 // Declare array using range with step
 let gapof5 = [|0..5..100|]
+
+// Declare array using expression
+let array3 = [| for i in 1 .. 10 -> i * i |]
+printf "%A" array3
+
+// Accessing array element
+let first1000 = [|1..1000|]
+printf  "%d" first1000.[0] // first element
+printf  "%A" first1000.[0..1] //first 2 elements
