79fe3b43c192a7ab60d6f24cce3a9a6e59855940
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-20 14:48:19 +0530

    Refactor code

--- F#/Basic/CommandLineArguments/cla.fs
+++ F#/Basic/CommandLineArguments/cla.fs
@@ -5,10 +5,10 @@ open System
 //     printfn "%A" argv
 //     0 // return an integer exit code
 
-
 [<EntryPoint>]
 let main argv =
     let a = Console.ReadLine() |> int
     let b = Console.ReadLine() |> int
-    printfn "%d" (a+b)
+    printfn "%d" ( a + b )
+    Console.ReadKey() |> ignore
     0 // return an integer exit code
