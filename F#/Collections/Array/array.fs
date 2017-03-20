2bb89e61f9932f99ae49b5f5346d593ffb45c5aa
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-20 17:26:12 +0530

    Refactor code

--- F#/Collections/Array/array.fs
+++ F#/Collections/Array/array.fs
@@ -2,8 +2,18 @@
 
 let numbers = [| 10; 20; 30; 40; 50 |]
 
-printfn "Array elements are"
+// Iterating using for..in
+printfn "Print array element using for..in loop"
+
+for number in numbers do
+    printf "%d " number
+
+printfn "Print array element using Sequence"
 numbers |> Seq.iter (printf "%d ")
+
+printfn "Print array element using Array iter method"
+Array.iter(fun n -> printf "%d " n) numbers
+
 
 printfn "First element in the array %d" numbers.[0]
 
@@ -27,11 +37,35 @@ printfn "%A" first100
 // Declare array using range with step
 let gapof5 = [|0..5..100|]
 
-// Declare array using expression
+// Declare array using lambda expression
 let array3 = [| for i in 1 .. 10 -> i * i |]
 printf "%A" array3
 
+// Declare array using expression
+let array4 = [| for i in 1 .. 10 do yield i * i |]
+printf "%A" array4
+
 // Accessing array element
 let first1000 = [|1..1000|]
-printf  "%d" first1000.[0] // first element
-printf  "%A" first1000.[0..1] //first 2 elements
+printf "%d" first1000.[0] // first element
+printf "%A" first1000.[0..4] //first 5 elements
+
+// Array function - filter
+let squares = [|for i in 0..99 -> i * i|]
+
+let IsEven n = n % 2 = 0
+
+let evenSquares = Array.filter (fun x -> IsEven x) squares
+
+printfn "%A" evenSquares
+
+// sort
+let fruits2 =
+    [|
+        "Banana"
+        "Apple"
+        "Orange"
+        "Mango"
+        "Pinapple"
+    |]
+let sortedFruits = Array.sort fruits2
