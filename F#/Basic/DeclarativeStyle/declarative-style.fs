95e2e8613fa5ac48eaff060d63fafdc1eaf3ce9d
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-20 14:48:11 +0530

    Refactor code

--- F#/Basic/DeclarativeStyle/declarative-style.fs
+++ F#/Basic/DeclarativeStyle/declarative-style.fs
@@ -1,18 +1,20 @@
-// sqaure function accepting an int and returning an int
-let sqaure x = x * x;
-let sqaured = List.map sqaure [1;2;3]
-for i in sqaured do
+module fsharp.basic
+
+// square function accepting an int and returning an int
+let squareInt x = x * x;
+let squaredIntList = List.map squareInt [1; 2;3]
+
+for i in squaredIntList do
    printfn "%d" i
 
-
-// sqaure function accepting an float and returning an float
-let square2 (x:float) : float = x * x
-let sqaured2 = List.map square2 [1.2;2.5;3.9]
-for j in sqaured2 do
+// square function accepting a float and returning a float
+let squareFloat (x:float) : float = x * x
+let sqauredFloatList = List.map squareFloat [1.2; 2.5; 3.9]
+for j in sqauredFloatList do
    printfn "%f" j
 
 // doubling list values using map function
-let doubles = List.map((*) 2) [1;2;3;4]
+let doubles = List.map((*) 2) [1; 2; 3; 4]
 for k in doubles do
     printfn "%d" k
 
