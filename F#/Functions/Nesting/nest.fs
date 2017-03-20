c0dcb397b7c69fb3d8415d4b1b4bc8cc3c62c62c
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-20 14:47:24 +0530

    Refactor Code

--- F#/Functions/Nesting/nest.fs
+++ F#/Functions/Nesting/nest.fs
@@ -1,7 +1,10 @@
-// Print a dynamic series 
+// Print a dynamic series
 let PrintNumbersWithSquares min max =
-    let square n = // local function!
-        n * n
+
+    let square n = n * n // local function!
+
     for n in min..max do
         printfn "%d %d" n (square n)
     printfn " "
+
+PrintNumbersWithSquares  1 10
