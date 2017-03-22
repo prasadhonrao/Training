b1aa8e071e8f80b2099fc0c3547b2c2a7d7562e7
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-22 13:05:22 +0530

    Add constructor example

---
+++ F#/OOP/Constructor/constructor.fs
@@ -0,0 +1,14 @@
+module OOP
+
+open System
+
+type Person(firstName: string, lastName: string) =
+    let validateString str =
+        if String.IsNullOrWhiteSpace str then
+            raise (ArgumentException())
+    do
+        validateString firstName
+        validateString lastName
+
+    member val FirstName = firstName with get, set
+    member val LastName = lastName with get, set
