57f5d3efc8ed2911bc2a0c0ce100720f14db8dd4
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-23 15:28:22 +0530

    Refactor code

--- F#/Functions/Chaining/chain.fs
+++ F#/Functions/Chaining/chain.fs
@@ -1,6 +1,5 @@
-let listSum = [1;2;3;4]
+let listSum = [1; 2; 3; 4]
                 |> List.filter (fun i -> i % 2 = 0)
-                |> List.map((*)2)
-                |> List.sum
+                |> List.sumBy((*)2)
 
 printfn "%d" listSum
