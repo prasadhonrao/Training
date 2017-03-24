3a4c90eccd0043d9c3d8c91efad5ba2b67e213e5
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-24 14:45:08 +0530

    Refactor code

--- F#/OOP/Class/class.fs
+++ F#/OOP/Class/class.fs
@@ -1,13 +1,18 @@
 //namespace OOP
 
 type Person(firstName: string, lastName: string) =
+    // default constructor
+    new() = Person("", "")
     member this.FirstName = firstName
     member __.LastName = lastName // this can be replaced using __
+    member this.GetFullName = firstName + " " + lastName
 
 let p1 = Person("Prasad", "Honrao")
 printfn "%A" p1
 
 let p2 = Person(firstName = "Prasad", lastName = "Honrao")
 printfn "%A" p2
+printfn "Full name : %s" p2.GetFullName
 
-
+let p3 = Person()
+printfn "Full name : %s" p3.GetFullName
