9775d18991ec168775208e78a99a90737e251df8
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-03-28 10:43:50 +0530

    Add type inference

---
+++ TypeScript/TypeInference/inference.ts
@@ -0,0 +1,19 @@
+var any1;
+console.log(any1);
+
+var number1 = 10;
+var number2: number = 20;
+
+console.log(typeof(number1) + " " + number1);
+console.log(number2);
+
+var number3 = number1 + 100;
+console.log(number3);
+
+var str = "typescript is powerful";
+console.log(typeof(str) + " " + str);
+
+var isOld = true;
+console.log(typeof(isOld) + " " + isOld);
+
+//var invalidNumber: number = "Hello" + 20; // Error!
