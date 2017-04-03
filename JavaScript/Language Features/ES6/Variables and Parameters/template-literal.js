f9b8662caaed4b71b070a22ddcba713bb1ceef6a
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-04-03 15:22:32 +0530

    Refactor code

--- JavaScript/Language Features/ES6/Variables and Parameters/template-literal.js
+++ JavaScript/Language Features/ES6/Variables and Parameters/template-literal.js
@@ -4,3 +4,8 @@ var id = 10;
 // string must be enclose using backtick
 var url = `http://gaana.com/${category}/${id}`;
 console.log(url);
+
+// multiline string can be declared
+var multipleLines = `Roses are red
+                    Voilets are blue`;
+console.log(multipleLines);
