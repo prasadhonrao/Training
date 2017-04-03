0febbd68c183153a45c772ed3d8ac1a64156af84
Prasad Honrao <Honrao.Prasad@Gmail.com>
2017-04-03 17:38:42 +0530

    Add loading animation

--- EmberJS/loopylog/app/routes/production.js
+++ EmberJS/loopylog/app/routes/production.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+const Promise = Ember.RSVP.Promise;
 
 export default Ember.Route.extend({
+  model() {
+  return new Promise(function(resolve) {
+      setTimeout(function() {
+        resolve(true);
+      }, 2000);
+    });
+  }
 });
