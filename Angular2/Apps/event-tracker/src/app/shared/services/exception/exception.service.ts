c55b0d727a9535d0f2074f452691b016579f7f9b
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-04-14 13:11:17 +0530

    Add exception service

--- Angular2/Apps/event-tracker/src/app/shared/services/exception/exception.service.ts
+++ Angular2/Apps/event-tracker/src/app/shared/services/exception/exception.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class ExceptionService {
 
   constructor() { }
+
+   catchBadResponse: (errorResponse: any) => Observable<any> = (errorResponse: any) => {
+      // TODO : show error message using popup or toaster
+      const res = <Response>errorResponse;
+      const err = res.json();
+      const emsg = err ?
+      (err.error ? err.error : JSON.stringify(err)) :
+      (res.statusText || 'unknown error');
+      console.error(emsg);
+      return Observable.throw(err);
+  }
+
 
 }
