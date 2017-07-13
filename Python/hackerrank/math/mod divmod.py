d2e2e5f299d321194558a98192ef6bbda541ff9c
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-07-13 07:09:48 +0530

    Add HackerRank solution

---
+++ Python/hackerrank/math/mod divmod.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    
+    import math
+
+    first = int(input())
+    second = int(input())
+
+    print(first // second)
+    print(first % second)
+    print(divmod(first, second))
