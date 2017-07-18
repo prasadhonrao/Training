2b310f43694c78e78c162896de2c9306731df249
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-07-18 14:45:41 +0530

    Add HackerRank solution

--- Python/hackerrank/collections/collectsions.counter.py
+++ Python/hackerrank/collections/collectsions.counter.py
@@ -1,10 +1,25 @@
 # A counter is a container that stores elements as dictionary keys, and their 
 # counts are stored as dictionary values.
+
+# from collections import Counter
+# myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
+# print (Counter(myList))
+# print (Counter(myList).items())
+# print (Counter(myList).keys())
+# print (Counter(myList).values())
+
+
 from collections import Counter
 
-myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
+X = int(input())
+shoes = Counter(map(int, input().split()))
+N = int(input())
+income = 0
 
-print (Counter(myList))
-print (Counter(myList).items())
-print (Counter(myList).keys())
-print (Counter(myList).values())
+for _ in range(N):
+    size, price = map(int, input().split())
+    if shoes[size]: 
+        income += price
+        shoes[size] -= 1
+
+print(income)    
