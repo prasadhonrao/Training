b7c86564e8768f0cff9762a1695fe34aea4c631b
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-07-19 10:41:29 +0530

    Add azure service bus - queue example

---
+++ Azure/ServiceBus/Main/Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            var connection = "<Add endpoint connection string>";
+            var client = QueueClient.CreateFromConnectionString(connection);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine("Received : " + message.GetBody<string>());
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Azure/ServiceBus/Main/Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = "<Add endpoint connection string>";
+            var client = QueueClient.CreateFromConnectionString(connection);
+
+            var lastKey = ' ';
+            var count = 1;
+
+            while (lastKey != 'q')
+            {
+                var body = $"New Message {count++}";
+                BrokeredMessage message = new BrokeredMessage(body);
+                client.Send(message);
+
+                Console.WriteLine($"{body} sent");
+                lastKey = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
