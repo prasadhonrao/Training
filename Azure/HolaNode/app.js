b777386997e9793418b5d4cc9c45992cfc1c2a72
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-08-09 19:29:54 +0530

    Add HolaNode

---
+++ Azure/HolaNode/app.js
@@ -0,0 +1,13 @@
+
+
+var express = require('express'),
+  config = require('./config/config');
+
+var app = express();
+
+module.exports = require('./config/express')(app, config);
+
+app.listen(config.port, function () {
+  console.log('Express server listening on port ' + config.port);
+});
+
