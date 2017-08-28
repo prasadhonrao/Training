ad93a95a7f9f03dedbde745df74db1c02e07eafa
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-08-28 15:45:23 +0530

    Rafactor code

--- Python/types/none.py
+++ Python/types/none.py
@@ -1,4 +1,11 @@
+# Python never prints None value
 print(None)
 
 isNone = None
-print(isNone is None)
+print(isNone is None) # Check for None using 'is' operator
+
+# None to check in boolean condition
+if(None):
+    print("None is Truthy")
+else:
+    print("None is Falsy")
