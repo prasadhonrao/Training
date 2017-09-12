af03ec28fa8b022accdda72a7f420eb8016ab548
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-09-12 10:21:12 +0530

    Refactor code

--- Python/collection/list/list.py
+++ Python/collection/list/list.py
@@ -28,6 +28,9 @@ print(languages[0:]) #slice till the end
 print(languages[1:3])
 print(languages[-2:])
 
+# reverse a list using slice
+print(languages[::-1])
+
 # append to list
 languages = ["Python", "Scala", "Haskell", "F#"]
 languages.append("Clojure")
@@ -55,3 +58,18 @@ print("C#" in languages)
 languages = ["Python", "Scala", "Haskell", "F#"]
 del languages[0]
 print("Python exist? {0}".format("Python" in languages))
+
+# List sort
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse=True)
+
+# Print out full_sorted
+print(full_sorted)
