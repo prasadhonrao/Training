c148f8f6906a14fbec564ce9b8259f9beb96a198
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-09-12 10:21:23 +0530

    Refactor code

--- Python/oop/class-static-variables.py
+++ Python/oop/class-static-variables.py
@@ -6,13 +6,13 @@ class Student:
 
     #constructor
     def __init__(self, name, id=0):
-        self.name = name
-        self.id = id
+        self._name = name
+        self._id = id
         students.append(self)
 
     #override toString
     def __str__(self):
-        return "Student " + self.name.capitalize()
+        return "Student " + self._name.capitalize()
 
     def get_school_name(self):
         return self.school_name
