7ed455e53df252699f532d838b0e610b0c0da18e
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-09-18 17:23:49 +0530

    Add python chart examples

--- Python/matplotlib/line-chart.py
+++ Python/matplotlib/line-chart.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 year = [1950, 1970, 1990, 2010]
 population = [2.159, 3.692, 5.263, 6.972]
 
+# add chart metadata like title and axis
+
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.title('World Population Projection')
+
 # plt.plot(year, population)  #line chart
 plt.scatter(year, population) #scatter chart
 
