7d5da0a9dad9a770831d92ecb84db5ca6a99cbc5
PrasadHonrao <Honrao.Prasad@Gmail.com>
2017-12-20 08:27:23 +0530

    Add NameValueCollection extension method

---
+++ C#/Language Features/Extensions/Library/NameValueCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace Extensions.Library
+{
+    public static class NameValueCollectionExtensions
+    {
+        // Usage: var qs = collection.ToQueryString(); // returns name=bob&age=15&location=Columbus
+        public static string ToQueryString(this NameValueCollection nvc)
+        {
+            return string.Join("&",
+                nvc.AllKeys.Select(
+                    key => string.Format("{0}={1}",
+                        HttpUtility.UrlEncode(key),
+                        HttpUtility.UrlEncode(nvc[key]))));
+        }
+    }
+}
