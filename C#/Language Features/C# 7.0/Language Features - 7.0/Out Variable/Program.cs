960dc292a9e52efa45779ce3c378dabe5063d081
PrasadHonrao <Honrao.Prasad@Gmail.com>
2018-01-03 16:17:29 +0530

    Add C# 7 language features

---
+++ C#/Language Features/C# 7.0/Language Features - 7.0/Out Variable/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace Out_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Traditional Way - Before C# 7
+
+            // First declare out variable
+            int hour;
+            int minutes;
+            int seconds;
+            GetTime(out hour, out minutes, out seconds);
+            WriteLine($"{hour}:{minutes}:{seconds}");
+
+            #endregion
+
+            #region C# 7 way
+
+            GetTime(out int hour2, out int minutes2, out int seconds2);
+            WriteLine($"{hour2}:{minutes2}:{seconds2}");
+
+            #endregion
+
+            ReadLine();
+        }
+
+        public static void GetTime(out int hour, out int minutes, out int seconds)
+        {
+            hour = 5;
+            minutes = 25;
+            seconds = 33;
+        }
+    }
+}
---
+++ C#/Language Features/C# 7.0/Language Features - 7.0/Pattern Matching/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+namespace Pattern_Matching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSum(10);
+            PrintSum2("10");
+            ReadLine();
+        }
+
+        static void PrintSum(object input)
+        {
+            if (input is null) return; // constant pattern
+
+            if (!(input is int i)) return; // type pattern (int)
+
+            int sum = 0;
+
+            for (int j = 0; j <= i; j++)
+            {
+                sum += j;
+            }
+
+            WriteLine($"Sum of 1 to {i} is {sum}");
+        }
+
+        static void PrintSum2(object input)
+        {
+            if (input is null) return; // constant pattern
+
+            if (input is int i || input is string s && int.TryParse(s, out i)) // type pattern
+            {
+                int sum = 0;
+
+                for (int j = 0; j <= i; j++)
+                {
+                    sum += j;
+                }
+                WriteLine($"Sum of 1 to {i} is {sum}");
+
+            }
+        }
+    }
+}
