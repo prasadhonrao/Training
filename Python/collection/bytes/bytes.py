903c453e6bcb5f8fd24b11a809d8c8fd557e5373
PrasadHonrao <Honrao.Prasad@Gmail.com>
2018-01-30 15:56:40 +0530

    Refactor code

--- Python/collection/bytes/bytes.py
+++ Python/collection/bytes/bytes.py
@@ -1,8 +1,10 @@
-val = 'some bytes'
+message = b'I got few bytes'
+print("{0}".format(message))
+print("{0}".format(len(message)))
 
+val = "Καλημέρα κόσμε"
 data = val.encode('utf-8') # encode string to byte array
 print(data)
-print(len(data))
 
 decoded = data.decode('utf-8') # decode byte to string
 print(decoded)
--- Python/collection/str/str.py
+++ Python/collection/str/str.py
@@ -3,27 +3,27 @@ message1 = 'python ' + 'language 1'
 print(message1)
 
 # Declaration and concatenation
-message1 = 'python ' 'language 2'
-print(message1)
-
-# Declaration and concatenation on separate line
-message2 = 'python '
-message2 += 'language 3'
+message2 = 'python ' 'language 2'
 print(message2)
 
-# Multiline string using '''
-message3 = '''python
-           language 4'''
+# Declaration and concatenation on separate line
+message3 = 'python '
+message3 += 'language 3'
 print(message3)
 
-# String comparison
-print(message1 == message2)
-print(message1 is message2)
+# Multiline string using '''
+message4 = '''python
+           language 4'''
+print(message4)
 
 # Adding escape sequence
 escape = 'Escape Sequence Character \' !!!'
 print(escape)
 
 # for path variables
-systemPath = r'c:\Windows\System32'
+systemPath = r'c:\Windows\System32' # r prefix indicates raw string
 print(systemPath)
+
+# String comparison
+print(message1 == message2)
+print(message1 is message2)
