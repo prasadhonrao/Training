5bf8e40fbad68428d7dc983e7c01688ce87f2177
Prasad Honrao <Honrao.Prasad@Gmail.com>
2018-04-05 16:21:13 +0530

    Move code to Python repo

--- Python/comprehension/predicate.py
+++
@@ -1,6 +0,0 @@
-def is_even(x):
-    if (x % 2 == 0):
-        return True
-    return False
-
-print([x for x in range(100) if is_even(x)])
--- Python/introduction/helloworld.py
+++
@@ -1,9 +0,0 @@
-print("Hello, Python World!")
-
-x = 10
-print(x)
-print(x * 10)
-
-print(2  + 2)
-
-print(1 < 2)
