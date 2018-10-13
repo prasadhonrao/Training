52d9e607e20a6096ea07541578a1adf33b877009
Prasad Honrao <Honrao.Prasad@Gmail.com>
2018-10-13 14:04:26 +0530

    Remove unused code

--- Angular2/Apps/event-tracker/src/app/shared/pipes/init-caps/init-caps.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InitCapsPipe } from './init-caps.pipe';
-
-describe('InitCapsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new InitCapsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
